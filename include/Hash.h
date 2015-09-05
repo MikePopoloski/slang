027d81df76eda3fb1922e6898b19e81ed181344f
MikePopoloski <mike@popoloski.com>
2015-09-05 11:42:31 -0400

    Adding 64-bit hash function.

--- include/Hash.h
+++ include/Hash.h
@@ -3,5 +3,6 @@
 namespace slang {
 
 uint32_t xxhash32(const void* input, size_t len, uint32_t seed);
+uint64_t xxhash64(const void* input, size_t len, uint64_t seed);
 
 }
--- source/Hash.cpp
+++ source/Hash.cpp
@@ -2,8 +2,33 @@
 
 namespace slang {
 
-// pulled from the xxHash project:
-// https://github.com/Cyan4973/xxHash
+/*
+pulled from the xxHash project: https://github.com/Cyan4973/xxHash
+xxHash Library
+Copyright (c) 2012-2014, Yann Collet
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #define PRIME32_1   2654435761U
 #define PRIME32_2   2246822519U
@@ -11,11 +36,20 @@ namespace slang {
 #define PRIME32_4    668265263U
 #define PRIME32_5    374761393U
 
+#define PRIME64_1 11400714785074694791ULL
+#define PRIME64_2 14029467366897019727ULL
+#define PRIME64_3  1609587929392839161ULL
+#define PRIME64_4  9650029242287828579ULL
+#define PRIME64_5  2870177450012600261ULL
+
 #define XXH_rotl32(x,r) _rotl(x,r)
+#define XXH_rotl64(x,r) _rotl64(x,r)
 
 uint32_t xxhash32(const void* input, size_t len, uint32_t seed) {
     // we assume at least 4-byte alignment of input, which should
     // always be true for the systems we're running on
+    ASSERT(((size_t)input & 3) == 0);
+
     const uint8_t* p = (const uint8_t*)input;
     const uint8_t* bEnd = p + len;
     uint32_t h32;
@@ -48,8 +82,9 @@ uint32_t xxhash32(const void* input, size_t len, uint32_t seed) {
 
         h32 = XXH_rotl32(v1, 1) + XXH_rotl32(v2, 7) + XXH_rotl32(v3, 12) + XXH_rotl32(v4, 18);
     }
-    else
+    else {
         h32 = seed + PRIME32_5;
+    }
 
     h32 += (uint32_t)len;
     while (p + 4 <= bEnd) {
@@ -71,6 +106,104 @@ uint32_t xxhash32(const void* input, size_t len, uint32_t seed) {
     h32 ^= h32 >> 16;
 
     return h32;
+}
+
+uint64_t xxhash64(const void* input, size_t len, uint64_t seed) {
+    // we assume at least 8-byte alignment of input, which should
+    // always be true for the systems we're running on
+    ASSERT(((size_t)input & 7) == 0);
+
+    const uint8_t* p = (const uint8_t*)input;
+    const uint8_t* bEnd = p + len;
+    uint64_t h64;
+
+    if (len >= 32) {
+        const uint8_t* const limit = bEnd - 32;
+        uint64_t v1 = seed + PRIME64_1 + PRIME64_2;
+        uint64_t v2 = seed + PRIME64_2;
+        uint64_t v3 = seed + 0;
+        uint64_t v4 = seed - PRIME64_1;
+
+        do {
+            v1 += *(uint64_t*)p * PRIME64_2;
+            p += 8;
+            v1 = XXH_rotl64(v1, 31);
+            v1 *= PRIME64_1;
+            v2 += *(uint64_t*)p * PRIME64_2;
+            p += 8;
+            v2 = XXH_rotl64(v2, 31);
+            v2 *= PRIME64_1;
+            v3 += *(uint64_t*)p * PRIME64_2;
+            p += 8;
+            v3 = XXH_rotl64(v3, 31);
+            v3 *= PRIME64_1;
+            v4 += *(uint64_t*)p * PRIME64_2;
+            p += 8;
+            v4 = XXH_rotl64(v4, 31);
+            v4 *= PRIME64_1;
+        } while (p <= limit);
+
+        h64 = XXH_rotl64(v1, 1) + XXH_rotl64(v2, 7) + XXH_rotl64(v3, 12) + XXH_rotl64(v4, 18);
+
+        v1 *= PRIME64_2;
+        v1 = XXH_rotl64(v1, 31);
+        v1 *= PRIME64_1;
+        h64 ^= v1;
+        h64 = h64 * PRIME64_1 + PRIME64_4;
+
+        v2 *= PRIME64_2;
+        v2 = XXH_rotl64(v2, 31);
+        v2 *= PRIME64_1;
+        h64 ^= v2;
+        h64 = h64 * PRIME64_1 + PRIME64_4;
+
+        v3 *= PRIME64_2;
+        v3 = XXH_rotl64(v3, 31);
+        v3 *= PRIME64_1;
+        h64 ^= v3;
+        h64 = h64 * PRIME64_1 + PRIME64_4;
+
+        v4 *= PRIME64_2;
+        v4 = XXH_rotl64(v4, 31);
+        v4 *= PRIME64_1;
+        h64 ^= v4;
+        h64 = h64 * PRIME64_1 + PRIME64_4;
+    }
+    else {
+        h64 = seed + PRIME64_5;
+    }
+
+    h64 += (uint64_t)len;
+
+    while (p + 8 <= bEnd) {
+        uint64_t k1 = *(uint64_t*)p;
+        k1 *= PRIME64_2;
+        k1 = XXH_rotl64(k1, 31);
+        k1 *= PRIME64_1;
+        h64 ^= k1;
+        h64 = XXH_rotl64(h64, 27) * PRIME64_1 + PRIME64_4;
+        p += 8;
+    }
+
+    if (p + 4 <= bEnd) {
+        h64 ^= (uint64_t)(*(uint32_t*)p) * PRIME64_1;
+        h64 = XXH_rotl64(h64, 23) * PRIME64_2 + PRIME64_3;
+        p += 4;
+    }
+
+    while (p < bEnd) {
+        h64 ^= (*p) * PRIME64_5;
+        h64 = XXH_rotl64(h64, 11) * PRIME64_1;
+        p++;
+    }
+
+    h64 ^= h64 >> 33;
+    h64 *= PRIME64_2;
+    h64 ^= h64 >> 29;
+    h64 *= PRIME64_3;
+    h64 ^= h64 >> 32;
+
+    return h64;
 }
 
 }
