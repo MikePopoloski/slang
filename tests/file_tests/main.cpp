548433a779e8a34246ad006cf9fdffa753d4cfa7
Evan Miller <evanm@hudson-trading.com>
2017-02-10 14:45:39 -0500

    Reverted file_tests

--- tests/file_tests/main.cpp
+++ tests/file_tests/main.cpp
@@ -1,32 +1,40 @@
 #include <cstdlib>
 #include <string>
+#include <vector>
 
 #include "slang.h"
 
 using namespace slang;
 using namespace std::literals;
 
-static const char RelativeTestPath[] = "/Volumes/hrt/surge/testing";
+static const char RelativeTestPath[] = "tests/file_tests/corpus/include";
 
 int main() {
     // run through all external files in our corpus and make sure they parse without error
     SourceManager sourceManager;
-    sourceManager.addUserDirectory(StringRef("/Volumes/hrt/dev/fpga/include"s));
+    sourceManager.addUserDirectory(StringRef(RelativeTestPath));
 
     DiagnosticWriter diagWriter(sourceManager);
+    std::vector<SyntaxTree> trees;
+    Diagnostics diags;
+    DeclarationTable table {diags};
+
     int errors = 0;
+    int modules = 0;
     int files = 0;
     for (auto& p : getFilesInDirectory(RelativeTestPath)) {
-        if (p.str().compare(p.str().size() - 3, 3, ".sv")) continue;
-        printf("Parsing '%s'\n", p.str().c_str());
         SyntaxTree tree = SyntaxTree::fromFile(StringRef(p.str()), sourceManager);
         if (!tree.diagnostics().empty()) {
             printf("Parsing '%s'\n", p.str().c_str());
             printf("%s\n\n", diagWriter.report(tree.diagnostics()).c_str());
         }
-
+        //printf("%s", ((CompilationUnitSyntax*)tree.root())->
+          //  toString(SyntaxToStringFlags::IncludeTrivia).c_str());
         errors += tree.diagnostics().count();
         files++;
+
+        trees.emplace_back(std::move(tree));
+        table.addSyntaxTree(&trees.back());
 
         //if (errors > 100)
         //  break;
@@ -34,4 +42,17 @@ int main() {
 
     printf("\n");
     printf("Finished parsing %d files; %d errors found\n\n", files, errors);
+
+    printf("creating model ...\n");
+    BumpAllocator alloc {4096};
+    SemanticModel model {alloc, diags, table};
+
+    printf("iterating over top-level modules:\n");
+    for (const auto modulep : table.getTopLevelModules()) {
+        auto inst = model.makeImplicitInstance(modulep);
+        modules++;
+    }
+
+    printf("\n");
+    printf("Finished elaborating %d modules; %d errors found\n\n", modules, errors);
 }
