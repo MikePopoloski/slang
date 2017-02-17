465c310f4724e4799b07572f0f11e9ee7d46c1a9
Evan Miller <evanm@hudson-trading.com>
2017-02-17 11:43:03 -0500

    Support empty function arguments

--- source/Parser_expressions.cpp
+++ source/Parser_expressions.cpp
@@ -689,6 +689,10 @@ ArgumentListSyntax* Parser::parseArgumentList() {
 }
 
 ArgumentSyntax* Parser::parseArgument() {
+    // check for empty arguments
+    if (peek(TokenKind::Comma)) {
+        return nullptr;
+    }
     // check for named arguments
     if (peek(TokenKind::Dot)) {
         auto dot = consume();
