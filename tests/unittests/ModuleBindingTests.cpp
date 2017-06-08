487d03bd90be8add3741f57cc6908dd9b9673977
MikePopoloski <mike@popoloski.com>
2017-06-08 19:21:34 -0400

    Fix param overrides, missing loop generate blocks

--- tests/unittests/ModuleBindingTests.cpp
+++ tests/unittests/ModuleBindingTests.cpp
@@ -145,7 +145,7 @@ endmodule
     REQUIRE(instance.module.members().count() == 10);
 
     for (uint32_t i = 0; i < 10; i++) {
-        const auto& leaf = instance.member<GenerateBlockSymbol>(i).member<ModuleInstanceSymbol>(0);
+        const auto& leaf = instance.member<GenerateBlockSymbol>(i).member<ModuleInstanceSymbol>(1);
         const auto& foo = leaf.module.lookup<ParameterSymbol>("foo");
         CHECK(foo.value().integer() == i);
     }
