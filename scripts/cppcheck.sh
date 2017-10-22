66a016516529a12cd620cb7bb5ecea636d45aab2
MikePopoloski <mike@popoloski.com>
2017-10-22 10:35:39 -0400

    Fail the build if cppcheck has issues

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ev
-cppcheck-1.81/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=scripts/cppcheck_suppressions.txt
+cppcheck-1.81/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --error-exitcode=42 --suppressions-list=scripts/cppcheck_suppressions.txt
