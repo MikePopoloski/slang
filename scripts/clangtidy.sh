e51a30c245d0f28e71619d5386eda44e141a8a55
MikePopoloski <mike@popoloski.com>
2017-10-25 22:07:06 -0400

    Update clang-tidy script

--- scripts/clangtidy.sh
+++ scripts/clangtidy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -v
+clang-tidy -version
 FILES=$(find source -type f -name '*.cpp')
 for f in $FILES; do
-	clang-tidy $f -quiet -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
+	clang-tidy $f -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
 done
