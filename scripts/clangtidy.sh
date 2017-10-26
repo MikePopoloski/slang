710713f6e3d8955512ef5ec8c5bf0ff91c9492c5
MikePopoloski <mike@popoloski.com>
2017-10-26 07:24:56 -0400

    Use clang-tidy 5.0 instead of the default old version

--- scripts/clangtidy.sh
+++ scripts/clangtidy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -v
-clang-tidy -version
+/usr/lib/llvm-5.0/bin/clang-tidy -version
 FILES=$(find source -type f -name '*.cpp')
 for f in $FILES; do
-	clang-tidy $f -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
+	/usr/lib/llvm-5.0/bin/clang-tidy $f -quiet -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
 done
