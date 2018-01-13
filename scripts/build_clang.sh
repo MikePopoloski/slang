fd412183476759916ab4e74a82f10b94cdf3b0ee
MikePopoloski <mike@popoloski.com>
2018-01-13 13:41:20 -0500

    Fix build directories

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
-make -C /slang/build/projects/gmake-linux-clang -j 4 CXX=clang++-5.0
-/slang/build/linux64_clang/bin/unittestsDebug
+cd /slang
+make -C build/projects/gmake-linux-clang -j 4 CXX=clang++-5.0
+build/linux64_clang/bin/unittestsDebug
 FILES=$(find source -type f -name '*.cpp')
 for f in $FILES; do
 	/usr/lib/llvm-5.0/bin/clang-tidy $f -quiet -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
-make -C /slang/build/projects/gmake-linux -j 4 CXX=g++-7
-/slang/build/linux64_gcc/bin/unittestsDebug;
+cd /slang
+make -C build/projects/gmake-linux -j 4 CXX=g++-7
+build/linux64_gcc/bin/unittestsDebug;
 /tmp/cppcheck/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=cppcheck_suppressions.txt
+bash <(curl -s https://codecov.io/bash) || echo 'Codecov failed to upload'
