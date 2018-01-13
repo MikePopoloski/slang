0dc5bb9517eb89d560b7355387a440f92a45c541
MikePopoloski <mike@popoloski.com>
2018-01-13 12:46:03 -0500

    Fix paths

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
-make -C ../build/projects/gmake-linux-clang -j 4 CXX=clang++-5.0
-../build/linux64_clang/bin/unittestsDebug
+make -C /slang/build/projects/gmake-linux-clang -j 4 CXX=clang++-5.0
+/slang/build/linux64_clang/bin/unittestsDebug
 FILES=$(find source -type f -name '*.cpp')
 for f in $FILES; do
 	/usr/lib/llvm-5.0/bin/clang-tidy $f -quiet -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
