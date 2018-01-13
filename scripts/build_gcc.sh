c60f93fa404e1063f52ab5666c0be666aa8f2005
MikePopoloski <mike@popoloski.com>
2018-01-13 14:55:57 -0500

    Update path to suppressions file

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -3,5 +3,5 @@ set -ev
 cd /slang
 make -C build/projects/gmake-linux -j 4 CXX=g++-7
 build/linux64_gcc/bin/unittestsDebug;
-/tmp/cppcheck/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=cppcheck_suppressions.txt
+/tmp/cppcheck/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=scripts/cppcheck_suppressions.txt
 bash <(curl -s https://codecov.io/bash) || echo 'Codecov failed to upload'
