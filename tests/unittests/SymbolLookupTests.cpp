110965ed51516c1dc44fc56f599fde97b6cfab5c
MikePopoloski <mike@popoloski.com>
2018-02-17 11:29:49 -0500

    Add package lookup tests

--- tests/unittests/SymbolLookupTests.cpp
+++ tests/unittests/SymbolLookupTests.cpp
@@ -112,6 +112,82 @@ endmodule
     CHECK(diags[0].notes[2].code == DiagCode::NoteDeclarationHere);
 }
 
+TEST_CASE("Wildcard import lookup 3", "[symbols:lookup]") {
+    auto tree = SyntaxTree::fromText(R"(
+package p;
+    int x;
+endpackage
+
+package p2;
+    int x;
+endpackage
+
+module top;
+    import p::*;
+    if (1) begin : b
+        initial x = 1; // Imports p::x, not p2::x
+        import p2::*;
+    end
+endmodule
+)");
+
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+
+    const auto& top = *compilation.getRoot().topInstances[0];
+    const auto& x = top.memberAt<GenerateBlockSymbol>(1)
+                       .memberAt<ProceduralBlockSymbol>(0)
+                       .getBody()->as<ExpressionStatement>()
+                       .expr.as<BinaryExpression>()
+                       .left().as<NamedValueExpression>().symbol;
+
+    auto p_x = compilation.getPackage("p")->find("x");
+    auto p2_x = compilation.getPackage("p2")->find("x");
+
+    REQUIRE(p_x);
+    REQUIRE(p2_x);
+    CHECK(&x == p_x);
+    CHECK(&x != p2_x);
+
+    NO_COMPILATION_ERRORS;
+}
+
+TEST_CASE("Wildcard import lookup 4", "[symbols:lookup]") {
+    auto tree = SyntaxTree::fromText(R"(
+package p1;
+    function int f();
+        return 1;
+    endfunction
+endpackage
+
+package p2;
+    function int f();
+        return 2;
+    endfunction
+endpackage
+
+module top;
+    import p2::*;
+    int x;
+    if (1) begin : b
+        parameter int x = f();
+    end
+    import p1::*;
+endmodule
+)");
+
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+
+    const auto& top = *compilation.getRoot().topInstances[0];
+    const auto& x = top.memberAt<GenerateBlockSymbol>(2)
+                       .memberAt<ParameterSymbol>(0);
+
+    CHECK(x.getValue().integer() == 2);
+
+    NO_COMPILATION_ERRORS;
+}
+
 TEST_CASE("Package references", "[symbols:lookup]") {
     auto tree = SyntaxTree::fromText(R"(
 package ComplexPkg;
@@ -152,4 +228,58 @@ endmodule
     REQUIRE(diags.size() == 2);
     CHECK(diags[0].code == DiagCode::UndeclaredIdentifier);
     CHECK(diags[1].code == DiagCode::UndeclaredIdentifier);
+}
+
+TEST_CASE("Package references 2", "[symbols:lookup]") {
+    auto tree = SyntaxTree::fromText(R"(
+package p;
+    typedef enum { FALSE, TRUE } BOOL;
+    const BOOL c = FALSE;
+endpackage
+
+package q;
+    const int c = 0;
+endpackage
+
+module m1;
+    initial begin
+        import p::*;
+        import q::*;
+    end
+
+    initial begin
+        import p::*;
+        import q::*;
+        int x = c; // error
+    end
+endmodule
+
+module m2;
+    initial begin
+        int c;
+        import p::c; // error
+    end
+
+    initial begin
+        import p::c;
+        import q::c; // error
+    end
+
+    initial begin
+        import q::*;
+        int x = c;
+        import p::c; // makes line above error
+    end
+endmodule
+)");
+
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+
+    Diagnostics diags = compilation.getAllDiagnostics();
+    REQUIRE(diags.size() == 4);
+    CHECK(diags[0].code == DiagCode::AmbiguousWildcardImport);
+    CHECK(diags[1].code == DiagCode::RedefinitionDifferentSymbolKind);
+    CHECK(diags[2].code == DiagCode::Redefinition);
+    CHECK(diags[3].code == DiagCode::ImportNameCollision);
 }
