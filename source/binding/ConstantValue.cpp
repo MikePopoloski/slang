8d57d6611e92251acf0fa55291b8faa036b7cda8
MikePopoloski <mike@popoloski.com>
2018-03-18 09:39:28 -0400

    Fix noexcept specifier

--- source/binding/ConstantValue.cpp
+++ source/binding/ConstantValue.cpp
@@ -86,7 +86,8 @@ void LValue::store(const ConstantValue& newValue) {
 }
 
 LValue LValue::selectRange(ConstantRange range) const {
-    return std::visit([&range](auto&& arg) -> LValue {
+    return std::visit([&range](auto&& arg)
+                      noexcept(!std::is_same_v<std::decay_t<decltype(arg)>, Concat>) -> LValue {
         using T = std::decay_t<decltype(arg)>;
         if constexpr (std::is_same_v<T, std::monostate>)
             return nullptr;
