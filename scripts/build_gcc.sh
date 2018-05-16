b5e61a948a8043c7b59622faa8fab514f7e40a56
MikePopoloski <mike@popoloski.com>
2018-05-15 22:12:55 -0400

    Use gcov-8 for coverage

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -4,4 +4,4 @@ cd /slang
 make -C build/projects/gmake-linux -j 4 CXX=g++-8
 build/linux64_gcc/bin/unittestsDebug;
 /tmp/cppcheck/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=scripts/cppcheck_suppressions.txt
-bash <(curl -s https://codecov.io/bash) || echo 'Codecov failed to upload'
+bash <(curl -s https://codecov.io/bash) -x gcov-8 || echo 'Codecov failed to upload'
