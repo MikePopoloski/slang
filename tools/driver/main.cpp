7f1c5738f7baa378dfd6f671dae781041ff88eb0
MikePopoloski <mike@popoloski.com>
2018-06-10 17:23:03 -0400

    Expand driver options, add mode to print preprocessed output

--- tools/driver/main.cpp
+++ tools/driver/main.cpp
@@ -13,12 +13,68 @@
 
 using namespace slang;
 
+bool runPreprocessor(SourceManager& sourceManager, const Bag& options,
+                    const std::vector<SourceBuffer>& buffers) {
+    BumpAllocator alloc;
+    DiagnosticWriter writer(sourceManager);
+
+    bool success = true;
+    for (const SourceBuffer& buffer : buffers) {
+        Diagnostics diagnostics;
+        Preprocessor preprocessor(sourceManager, alloc, diagnostics, options);
+        preprocessor.pushSource(buffer);
+
+        SmallVectorSized<char, 32> output;
+        while (true) {
+            Token token = preprocessor.next();
+            token.writeTo(output, SyntaxToStringFlags::IncludePreprocessed | SyntaxToStringFlags::IncludeTrivia);
+            if (token.kind == TokenKind::EndOfFile)
+                break;
+        }
+
+        if (diagnostics.empty())
+            printf("%s:\n", std::string(sourceManager.getRawFileName(buffer.id)).c_str());
+        else {
+            printf("%s", writer.report(diagnostics).c_str());
+            success = false;
+        }
+
+        printf("==============================\n%s\n", std::string(output.begin(), output.size()).c_str());
+    }
+    return success;
+}
+
+bool runCompiler(SourceManager& sourceManager, const Bag& options,
+                 const std::vector<SourceBuffer>& buffers) {
+
+    Compilation compilation;
+    for (const SourceBuffer& buffer : buffers)
+        compilation.addSyntaxTree(SyntaxTree::fromBuffer(buffer, sourceManager, options));
+
+    Diagnostics diagnostics = compilation.getAllDiagnostics();
+    DiagnosticWriter writer(sourceManager);
+    printf("%s\n", writer.report(diagnostics).c_str());
+
+    return diagnostics.empty();
+}
+
 int main(int argc, char** argv)
 try {
     std::vector<std::string> sourceFiles;
+    std::vector<std::string> includeDirs;
+    std::vector<std::string> includeSystemDirs;
+    std::vector<std::string> defines;
+    std::vector<std::string> undefines;
+
+    bool onlyPreprocess;
 
     CLI::App cmd("SystemVerilog compiler");
     cmd.add_option("files", sourceFiles, "Source files to compile");
+    cmd.add_option("-I,--include-directory", includeDirs, "Additional include search paths");
+    cmd.add_option("--include-system-directory", includeSystemDirs, "Additional system include search paths");
+    cmd.add_option("-D,--define-macro", defines, "Define <macro>=<value> (or 1 if <value> ommitted) in all source files");
+    cmd.add_option("-U,--undefine-macro", undefines, "Undefine macro name at the start of all source files");
+    cmd.add_flag("-E,--preprocess", onlyPreprocess, "Only run the preprocessor (and print preprocessed files to stdout)");
 
     try {
         cmd.parse(argc, argv);
@@ -27,34 +83,45 @@ try {
         return cmd.exit(e);
     }
 
-    // Initialize the source manager.
     SourceManager sourceManager;
+    for (const std::string& dir : includeDirs)
+        sourceManager.addUserDirectory(string_view(dir));
 
-    // Build the compilation out of each source file.
-    Compilation compilation;
+    for (const std::string& dir : includeSystemDirs)
+        sourceManager.addSystemDirectory(string_view(dir));
+
+    PreprocessorOptions ppoptions;
+    ppoptions.predefines = defines;
+    ppoptions.undefines = undefines;
+    ppoptions.predefineSource = "<command-line>";
+
+    Bag options;
+    options.add(ppoptions);
+
     bool anyErrors = false;
+    std::vector<SourceBuffer> buffers;
     for (const std::string& file : sourceFiles) {
-        std::shared_ptr<SyntaxTree> tree = SyntaxTree::fromFile(file, sourceManager);
-        if (!tree) {
+        SourceBuffer buffer = sourceManager.readSource(file);
+        if (!buffer) {
             printf("error: no such file or directory: '%s'\n", file.c_str());
             anyErrors = true;
             continue;
         }
 
-        compilation.addSyntaxTree(std::move(tree));
+        buffers.push_back(buffer);
     }
 
-    if (compilation.getSyntaxTrees().empty()) {
+    if (buffers.empty()) {
         printf("error: no input files\n");
         return 1;
     }
 
-    // Report diagnostics.
-    Diagnostics diagnostics = compilation.getAllDiagnostics();
-    DiagnosticWriter writer(sourceManager);
-    printf("%s\n", writer.report(diagnostics).c_str());
+    if (onlyPreprocess)
+        anyErrors |= !runPreprocessor(sourceManager, options, buffers);
+    else
+        anyErrors |= !runCompiler(sourceManager, options, buffers);
 
-    return !anyErrors && diagnostics.empty() ? 0 : 1;
+    return anyErrors ? 1 : 0;
 }
 catch (const std::exception& e) {
     printf("internal compiler error (exception): %s\n", e.what());
