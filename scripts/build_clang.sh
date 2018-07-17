2fb6cd2b65aab96da8feab5e55a96472fac1a738
B00Ze <buzz.sh@tut.by>
2018-07-17 22:16:39 +0300

    Fix path to unittest binary

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -3,7 +3,7 @@ set -ev
 export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-6.0/bin/llvm-symbolizer
 cd /slang
 make -C build -j 8
-build/tests/unittests/unittests
+build/bin/unittests
 bash <(curl -s https://codecov.io/bash) -x 'llvm-cov-6.0 gcov' -X gcovout || echo 'Codecov failed to upload'
 FILES=$(find source -type f -name '*.cpp')
 for f in $FILES; do
--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -2,5 +2,5 @@
 set -ev
 cd /slang
 make -C build -j 8
-build/tests/unittests/unittests
+build/bin/unittests
 /tmp/cppcheck/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=scripts/cppcheck_suppressions.txt
