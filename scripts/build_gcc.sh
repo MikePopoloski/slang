33039596c0aa51457d3cc3fe0a94d52d75ae979f
MikePopoloski <mike@popoloski.com>
2018-07-25 07:02:32 -0400

    Try to reenable gcov coverage

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -3,4 +3,5 @@ set -ev
 cd /slang
 make -C build -j 8
 build/bin/unittests
+bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
 /tmp/cppcheck/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=scripts/cppcheck_suppressions.txt
