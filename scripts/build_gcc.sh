622efeca0bad7c03ff9460a5546afd10d1810e86
MikePopoloski <mike@popoloski.com>
2018-07-28 14:37:01 -0400

    More tweaks to static analysis

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -3,7 +3,7 @@ set -ev
 cd /slang
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=g++-8 -DSLANG_COVERAGE=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--inconclusive;--suppressions-list=scripts/cppcheck_suppressions.txt" ..
+cmake -DCMAKE_CXX_COMPILER=g++-8 -DSLANG_COVERAGE=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--inconclusive;--suppressions-list=../scripts/cppcheck_suppressions.txt" ..
 make -j 8
 bin/unittests
 bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
