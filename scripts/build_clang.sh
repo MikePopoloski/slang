ce82c6223e58d1c477c6b2dd54a3057e9585b866
MikePopoloski <mike@popoloski.com>
2018-07-28 13:28:06 -0400

    Hide llvm-cov output in Clang CI build

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -4,7 +4,7 @@ export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-6.0/bin/llvm-symbolizer
 cd /slang
 make -C build -j 8
 build/bin/unittests
-bash <(curl -s https://codecov.io/bash) -x 'llvm-cov-6.0 gcov' -X gcovout || echo 'Codecov failed to upload'
+bash <(curl -s https://codecov.io/bash) -x 'llvm-cov-6.0 gcov' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
 FILES=$(find source -type f -name '*.cpp')
 for f in $FILES; do
 	/usr/lib/llvm-6.0/bin/clang-tidy $f -quiet -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
