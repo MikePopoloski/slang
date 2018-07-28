b3cd6fe0b9c0c9f719545216c7bd57e850ce97d7
MikePopoloski <mike@popoloski.com>
2018-07-28 14:21:44 -0400

    Run cppcheck and clang-tidy via cmake

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -2,10 +2,9 @@
 set -ev
 export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-6.0/bin/llvm-symbolizer
 cd /slang
-make -C build -j 8
-build/bin/unittests
+mkdir build
+cd build
+cmake -DCMAKE_CXX_COMPILER=clang++-6.0 -DSLANG_COVERAGE=ON -DSLANG_SANITIZERS=undefined,address -DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-6.0/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,misc-*,readability-* ..
+make -j 8
+bin/unittests
 bash <(curl -s https://codecov.io/bash) -x 'llvm-cov-6.0 gcov' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
-FILES=$(find source -type f -name '*.cpp')
-for f in $FILES; do
-	/usr/lib/llvm-6.0/bin/clang-tidy $f -quiet -checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto -- -Isource -Iexternal -I. -std=c++1z;
-done
--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ev
 cd /slang
-make -C build -j 8
-build/bin/unittests
+mkdir build
+cd build
+cmake -DCMAKE_CXX_COMPILER=g++-8 -DSLANG_COVERAGE=ON -DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--inconclusive;--suppressions-list=scripts/cppcheck_suppressions.txt ..
+make -j 8
+bin/unittests
 bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
-/tmp/cppcheck/cppcheck source -I. -Iexternal -Isource -q --enable=warning,performance,portability --inconclusive --suppressions-list=scripts/cppcheck_suppressions.txt
