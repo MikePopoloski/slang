13088d6ce30c2fb54e5dd60c5ccbc3dbee4a808a
MikePopoloski <mike@popoloski.com>
2018-09-10 20:19:07 -0400

    Use CTest for running tests in CI builds

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -6,5 +6,5 @@ mkdir build
 cd build
 cmake -DCMAKE_CXX_COMPILER=clang++-6.0 -DSLANG_COVERAGE=ON -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-6.0/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,misc-*" ..
 make -j 8
-bin/unittests
+ctest
 bash <(curl -s https://codecov.io/bash) -x 'llvm-cov-6.0 gcov' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -6,5 +6,5 @@ cd build
 s=$(realpath ../scripts/cppcheck_suppressions.txt)
 cmake -DCMAKE_CXX_COMPILER=g++-8 -DSLANG_COVERAGE=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--inconclusive;--suppressions-list=$s" ..
 make -j 8
-bin/unittests
+ctest
 bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
