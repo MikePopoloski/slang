2f6c6f1c6c0a47c61290fe71fea0f6428b4abdf6
MikePopoloski <mike@popoloski.com>
2018-09-18 21:13:46 -0400

    Attempt to fix Codecov paths

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -7,4 +7,5 @@ s=$(realpath ../scripts/cppcheck_suppressions.txt)
 cmake -DCMAKE_CXX_COMPILER=g++-8 -DSLANG_COVERAGE=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--inconclusive;--suppressions-list=$s" ..
 make -j 8
 ctest
+cd ..
 bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
