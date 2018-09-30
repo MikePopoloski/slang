14b01ac2e2a3283be3b8dcf21f4f3b5c8bddfacc
MikePopoloski <mike@popoloski.com>
2018-09-30 13:04:52 -0400

    Disable LLVM coverage for now

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -4,8 +4,7 @@ export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-6.0/bin/llvm-symbolizer
 cd /slang
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=clang++-6.0 -DSLANG_COVERAGE=ON -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-6.0/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,misc-*" ..
+cmake -DCMAKE_CXX_COMPILER=clang++-6.0 -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-6.0/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,misc-*" ..
 make -j 8
 ctest
 cd ..
-bash <(curl -s https://codecov.io/bash) -x 'llvm-cov-6.0 gcov' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
