ac2d5fde6521e9d443edcc12fe1f78ff12c1b7e0
MikePopoloski <mike@popoloski.com>
2018-09-30 18:44:58 -0400

    Suppress dumb clang analyzer warnings

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -4,7 +4,7 @@ export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-7/bin/llvm-symbolizer
 cd /slang
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=clang++-7 -DCMAKE_BUILD_TYPE=Debug -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-7/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,misc-*" ..
+cmake -DCMAKE_CXX_COMPILER=clang++-7 -DCMAKE_BUILD_TYPE=Debug -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-7/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,-bugprone-suspicious-semicolon,-clang-analyzer-cplusplus.NewDelete*,misc-*" ..
 make -j 8
 ctest
 cd ..
