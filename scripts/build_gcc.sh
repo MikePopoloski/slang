1a8bc87dcecea5d2927436b5d19bc5e6c4ba593d
MikePopoloski <mike@popoloski.com>
2018-09-30 17:48:45 -0400

    Make CI use Debug builds

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -4,7 +4,7 @@ cd /slang
 mkdir build
 cd build
 s=$(realpath ../scripts/cppcheck_suppressions.txt)
-cmake -DCMAKE_CXX_COMPILER=g++-8 -DSLANG_COVERAGE=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--suppressions-list=$s" ..
+cmake -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_BUILD_TYPE=Debug -DSLANG_COVERAGE=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--suppressions-list=$s" ..
 make -j 8
 ctest
 cd ..
