41b7fb8d50e5a6ceb0b902a6da1765c6c05fece7
MikePopoloski <mike@popoloski.com>
2018-12-27 14:49:04 -0500

    Experiment with Clang source-based coverage reporting

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -4,7 +4,10 @@ export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-7/bin/llvm-symbolizer
 cd /slang
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=clang++-7 -DCMAKE_BUILD_TYPE=Debug -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-7/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,-bugprone-suspicious-semicolon,-clang-analyzer-cplusplus.NewDelete*,-clang-analyzer-cplusplus.InnerPointer,misc-*" ..
+cmake -DCMAKE_CXX_COMPILER=clang++-7 -DCMAKE_BUILD_TYPE=Debug -DSLANG_COVERAGE=ON -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-7/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,-bugprone-suspicious-semicolon,-clang-analyzer-cplusplus.NewDelete*,-clang-analyzer-cplusplus.InnerPointer,misc-*" ..
 make -j 8
+export LLVM_PROFILE_FILE=%p.profraw
 ctest
-cd ..
+find . -name *.profraw -exec llvm-profdata-7 merge -o merged.profdata -sparse {} + ;
+llvm-cov-7 show bin/unittests -instr-profile=merged.profdata > coverage.txt
+bash <(curl -s https://codecov.io/bash) || echo 'Codecov failed to upload'
--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -4,8 +4,8 @@ cd /slang
 mkdir build
 cd build
 s=$(realpath ../scripts/cppcheck_suppressions.txt)
-cmake -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_BUILD_TYPE=Debug -DSLANG_COVERAGE=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--suppressions-list=$s" ..
+cmake -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_BUILD_TYPE=Debug "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--suppressions-list=$s" ..
 make -j 8
 ctest
 cd ..
-bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
+#bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
