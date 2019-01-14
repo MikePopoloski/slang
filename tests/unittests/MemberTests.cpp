7ba148323a92090361830439bfbebd6c42af2feb
MikePopoloski <mike@popoloski.com>
2019-01-13 19:54:09 -0500

    Add test for JSON dumping

--- tests/unittests/MemberTests.cpp
+++ tests/unittests/MemberTests.cpp
@@ -1,5 +1,7 @@
 #include "Test.h"
 
+#include <nlohmann/json.hpp>
+
 TEST_CASE("Nets") {
     auto tree = SyntaxTree::fromText(R"(
 module Top;
@@ -26,4 +28,55 @@ endmodule
     Compilation compilation;
     compilation.addSyntaxTree(tree);
     NO_COMPILATION_ERRORS;
+}
+
+TEST_CASE("JSON dump") {
+    auto tree = SyntaxTree::fromText(R"(
+interface I;
+    modport m();
+endinterface
+
+package p1;
+    parameter int BLAH = 1;
+endpackage
+
+module Top;
+    wire foo;
+    assign foo = 1;
+
+    I array [3] ();
+
+    always_comb begin
+    end
+
+    if (1) begin
+    end
+
+    for (genvar i = 0; i < 10; i++) begin
+    end
+
+    import p1::BLAH;
+
+    import p1::*;
+
+    logic f;
+    I stuff();
+    Child child(.i(stuff), .f);
+
+    function logic func(logic bar);
+    endfunction
+
+endmodule
+
+module Child(I.m i, input logic f = 1);
+endmodule
+)");
+
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+    NO_COMPILATION_ERRORS;
+
+    // This basic test just makes sure that JSON dumping doesn't crash.
+    json output = compilation.getRoot();
+    output.dump();
 }
