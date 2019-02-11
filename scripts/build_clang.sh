a4746c16084bac3579922d42b672a3111fe87e5c
MikePopoloski <mike@popoloski.com>
2019-02-10 21:48:08 -0500

    Reenable extended tests in CI builds

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -4,7 +4,7 @@ export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-7/bin/llvm-symbolizer
 cd /slang
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=clang++-7 -DCMAKE_BUILD_TYPE=Debug -DSLANG_COVERAGE=ON -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-7/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,-bugprone-suspicious-semicolon,-clang-analyzer-cplusplus.NewDelete*,-clang-analyzer-cplusplus.InnerPointer,misc-*" ..
+cmake -DCMAKE_CXX_COMPILER=clang++-7 -DCMAKE_BUILD_TYPE=Debug -DSLANG_COVERAGE=ON -DCI_BUILD=ON -DSLANG_SANITIZERS=undefined,address "-DCMAKE_CXX_CLANG_TIDY=/usr/lib/llvm-7/bin/clang-tidy;-quiet;-checks=-*,clang-analyzer-*,bugprone-*,performance-*,modernize-*,-modernize-use-auto,-modernize-raw-string-literal,-bugprone-suspicious-semicolon,-clang-analyzer-cplusplus.NewDelete*,-clang-analyzer-cplusplus.InnerPointer,misc-*" ..
 make -j 8
 export LLVM_PROFILE_FILE=%p.profraw
 ctest
--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -4,8 +4,6 @@ cd /slang
 mkdir build
 cd build
 s=$(realpath ../scripts/cppcheck_suppressions.txt)
-cmake -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_BUILD_TYPE=Debug "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--suppressions-list=$s" ..
+cmake -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_BUILD_TYPE=Debug -DCI_BUILD=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--suppressions-list=$s" ..
 make -j 8
 ctest
-cd ..
-#bash <(curl -s https://codecov.io/bash) -x 'gcov-8' -a '1> /dev/null 2> /dev/null' || echo 'Codecov failed to upload'
