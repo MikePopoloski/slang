a76f1c0b3e9f780f9b320e1e602454cd494ba4cb
MikePopoloski <mike@popoloski.com>
2019-02-19 18:59:28 -0500

    Remove cppcheck from the build

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -3,7 +3,6 @@ set -ev
 cd /slang
 mkdir build
 cd build
-s=$(realpath ../scripts/cppcheck_suppressions.txt)
-cmake -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_BUILD_TYPE=Debug -DCI_BUILD=ON "-DCMAKE_CXX_CPPCHECK=/tmp/cppcheck/cppcheck;-q;--enable=warning,performance,portability;--suppressions-list=$s" ..
+cmake -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_BUILD_TYPE=Debug -DCI_BUILD=ON ..
 make -j 8
 ctest
