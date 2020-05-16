2884ecaf2329486b3cc063c06629e3a1e78c941d
MikePopoloski <mike@popoloski.com>
2020-05-16 11:53:48 -0400

    Add simrt: a simulation runtime library

---
+++ source/simrt/print.cpp
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------
+// print.cpp
+// Runtime print output routines
+//
+// File is under the MIT license; see LICENSE for details
+//------------------------------------------------------------------------------
+#include <cstdint>
+#include <cstdio>
+
+void printChar(int8_t c) {
+    printf("Foo: %c\n", c);
+}
