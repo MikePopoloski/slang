673c3df115464a62b8c7288e190590cccf5aabff
Ti-Yen Yen <thingkingland@icloud.com>
2020-08-13 15:48:29 -0700

    Unit test for endless lop from unsupported statements. Skip token for unsupported statement in Parser::parseRandCaseStatement.

--- source/parsing/Parser_statements.cpp
+++ source/parsing/Parser_statements.cpp
@@ -688,7 +688,13 @@ RandCaseStatementSyntax& Parser::parseRandCaseStatement(NamedLabelSyntax* label,
     while (isPossibleExpression(peek().kind)) {
         auto& expr = parseExpression();
         auto colon = expect(TokenKind::Colon);
-        itemBuffer.append(&factory.randCaseItem(expr, colon, parseStatement()));
+        const auto loc = peek().location();
+        auto& stmt = parseStatement();
+        if (stmt.kind == SyntaxKind::EmptyStatement &&
+            stmt.as<EmptyStatementSyntax>().semicolon.isMissing() && loc == peek().location()) {
+            skipToken(std::nullopt);
+        }
+        itemBuffer.append(&factory.randCaseItem(expr, colon, stmt));
     }
 
     auto endcase = expect(TokenKind::EndCaseKeyword);
