db598ec448758493e59a8c18785e2a4089c7d867
MikePopoloski <mike@popoloski.com>
2022-07-31 21:09:36 -0400

    Add initial pyslang Python package structure and test folder

---
+++ pyslang/setup.py
@@ -0,0 +1,19 @@
+from skbuild import setup
+from setuptools import find_packages
+
+setup(
+    name="pyslang",
+    version="2.0.0",
+    description="Python bindings for slang, a library for compiling SystemVerilog",
+    author="Mike Popoloski",
+    license="MIT",
+    packages=find_packages(where="source"),
+    package_dir={"": "source"},
+    cmake_source_dir='../',
+    cmake_install_dir="source/pyslang",
+    cmake_install_target="pyslang-install-pylib",
+    cmake_args=['-DSLANG_INCLUDE_TESTS=OFF', '-DSLANG_INCLUDE_PYLIB=ON'],
+    include_package_data=True,
+    extras_require={"test": ["pytest"]},
+    python_requires=">=3.6",
+)
---
+++ pyslang/tests/test_numerics.py
@@ -0,0 +1,4 @@
+from pyslang import SVInt
+
+def test_numerics():
+    assert(SVInt(-3) == -3)
