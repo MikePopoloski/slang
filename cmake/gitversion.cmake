a52de33e36805c603d6310a962ccbb39b2b7277a
MikePopoloski <mike@popoloski.com>
2022-08-03 20:09:22 -0400

    auto-format all cmake files

--- cmake/gitversion.cmake
+++ cmake/gitversion.cmake
@@ -2,16 +2,14 @@ set(_build_version "unknown")
 
 find_package(Git)
 if(GIT_FOUND)
- 	execute_process(
-    	COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-    	WORKING_DIRECTORY "${local_dir}"
-    	OUTPUT_VARIABLE _build_version
-    	ERROR_QUIET
-    	OUTPUT_STRIP_TRAILING_WHITESPACE
-  	)
-  	message(STATUS "git hash: ${_build_version}")
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+    WORKING_DIRECTORY "${local_dir}"
+    OUTPUT_VARIABLE _build_version
+    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+  message(STATUS "git hash: ${_build_version}")
 else()
-  	message(STATUS "git not found")
+  message(STATUS "git not found")
 endif()
 
 configure_file(${infile} ${outfile} @ONLY)
