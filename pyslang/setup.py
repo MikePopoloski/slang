406a48f6cbddc32832fb6eb5d1b9fc26ad25cf69
MikePopoloski <mike@popoloski.com>
2022-08-07 14:49:17 -0400

    pyslang: add fallback file for storing release version for source tarball distributions

--- pyslang/setup.py
+++ pyslang/setup.py
@@ -1,28 +1,59 @@
 from subprocess import check_output
 
-from setuptools import find_packages
 from skbuild import setup
 
-command = "git describe --tags --long --dirty"
-fmt = "{tag}+{commitcount}.{gitsha}"
 
-git_version = check_output(command.split()).decode("utf-8").strip()
-if git_version.startswith("v"):
-    git_version = git_version[1:]
+def get_git_version():
+    command = "git describe --tags --long --dirty"
+    fmt = "{tag}+{commitcount}.{gitsha}"
 
-parts = git_version.split("-")
-assert len(parts) in (3, 4)
-dirty = len(parts) == 4
-tag, count, sha = parts[:3]
-if count == "0" and not dirty:
-    version = tag
-else:
-    version = fmt.format(tag=tag, commitcount=count, gitsha=sha)
-    if dirty:
-        version = version + ".dirty"
+    try:
+        git_version = check_output(command.split()).decode("utf-8").strip()
+    except Exception:
+        return None
+
+    if git_version.startswith("v"):
+        git_version = git_version[1:]
+
+    parts = git_version.split("-")
+    assert len(parts) in (3, 4)
+    dirty = len(parts) == 4
+    tag, count, sha = parts[:3]
+    if count == "0" and not dirty:
+        version = tag
+    else:
+        version = fmt.format(tag=tag, commitcount=count, gitsha=sha)
+        if dirty:
+            version = version + ".dirty"
+
+    return version
+
+
+def get_version():
+    # Try to read existing release version file.
+    try:
+        f = open("RELEASE-VERSION", "r")
+        fs_version = f.readlines()[0].strip()
+    except Exception:
+        fs_version = None
+
+    # Get the version as described by git, if present.
+    version = get_git_version()
+    if version is None:
+        version = fs_version
+
+    if version is None:
+        raise ValueError("Cannot find the version number!")
+
+    if version != fs_version:
+        f = open("RELEASE-VERSION", "w")
+        f.write("{}\n".format(version))
+
+    return version
+
 
 setup(
-    version=version,
+    version=get_version(),
     packages=[""],
     package_dir={"": "source"},
     cmake_source_dir="../",
