3cf9c68586da139d528f46eb774950c1eaa50898
MikePopoloski <mike@popoloski.com>
2022-08-07 13:55:17 -0400

    pyslang: set version number for distribution

--- pyslang/setup.py
+++ pyslang/setup.py
@@ -1,8 +1,28 @@
+from subprocess import check_output
+
 from setuptools import find_packages
 from skbuild import setup
 
+command = "git describe --tags --long --dirty"
+fmt = "{tag}+{commitcount}.{gitsha}"
+
+git_version = check_output(command.split()).decode("utf-8").strip()
+if git_version.startswith("v"):
+    git_version = git_version[1:]
+
+parts = git_version.split("-")
+assert len(parts) in (3, 4)
+dirty = len(parts) == 4
+tag, count, sha = parts[:3]
+if count == "0" and not dirty:
+    version = tag
+else:
+    version = fmt.format(tag=tag, commitcount=count, gitsha=sha)
+    if dirty:
+        version = version + ".dirty"
+
 setup(
-    version="2.0.0",
+    version=version,
     packages=[""],
     package_dir={"": "source"},
     cmake_source_dir="../",
