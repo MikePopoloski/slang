fd16f2ca983d4d83a8aea8532157f040a0afe5d6
MikePopoloski <mike@popoloski.com>
2022-08-16 20:41:20 -0400

    OS: add a mechanism to capture stdout/stderr for use with unit tests

--- include/slang/util/OS.h
+++ include/slang/util/OS.h
@@ -10,6 +10,7 @@
 #include <fmt/color.h>
 #include <vector>
 
+#include "slang/util/ScopeGuard.h"
 #include "slang/util/String.h"
 
 namespace slang {
@@ -40,13 +41,21 @@ public:
     /// Prints formatted text to stdout, handling Unicode conversions where necessary.
     template<typename... Args>
     static void print(string_view format, const Args&... args) {
-        fmt::vprint(stdout, format, fmt::make_format_args(args...));
+        if (capturingOutput) {
+            capturedStdout += fmt::vformat(format, fmt::make_format_args(args...));
+        }
+        else {
+            fmt::vprint(stdout, format, fmt::make_format_args(args...));
+        }
     }
 
     /// Prints colored formatted text to stdout, handling Unicode conversions where necessary.
     template<typename... Args>
     static void print(const fmt::text_style& style, string_view format, const Args&... args) {
-        if (showColorsStdout) {
+        if (capturingOutput) {
+            capturedStdout += fmt::vformat(format, fmt::make_format_args(args...));
+        }
+        else if (showColorsStdout) {
             fmt::vprint(stdout, style, format, fmt::make_format_args(args...));
         }
         else {
@@ -57,13 +66,21 @@ public:
     /// Prints formatted text to stderr, handling Unicode conversions where necessary.
     template<typename... Args>
     static void printE(string_view format, const Args&... args) {
-        fmt::vprint(stderr, format, fmt::make_format_args(args...));
+        if (capturingOutput) {
+            capturedStderr += fmt::vformat(format, fmt::make_format_args(args...));
+        }
+        else {
+            fmt::vprint(stderr, format, fmt::make_format_args(args...));
+        }
     }
 
     /// Prints colored formatted text to stderr, handling Unicode conversions where necessary.
     template<typename... Args>
     static void printE(const fmt::text_style& style, string_view format, const Args&... args) {
-        if (showColorsStderr) {
+        if (capturingOutput) {
+            capturedStderr += fmt::vformat(format, fmt::make_format_args(args...));
+        }
+        else if (showColorsStderr) {
             fmt::vprint(stderr, style, format, fmt::make_format_args(args...));
         }
         else {
@@ -71,11 +88,23 @@ public:
         }
     }
 
+    static auto captureOutput() {
+        capturedStdout.clear();
+        capturedStderr.clear();
+
+        capturingOutput = true;
+        return ScopeGuard([] { capturingOutput = false; });
+    }
+
+    static inline std::string capturedStdout;
+    static inline std::string capturedStderr;
+
 private:
     OS() = default;
 
     static inline bool showColorsStdout = false;
     static inline bool showColorsStderr = false;
+    static inline bool capturingOutput = false;
 };
 
 } // namespace slang
