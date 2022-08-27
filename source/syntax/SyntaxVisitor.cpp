eaded507cf52dc6e4976852fd8629cf47ce37eb5
HungMingWu <u9089000@gmail.com>
2022-08-27 21:33:01 +0800

    Enhance SyntaxVisitor functionality, support multiple operations (#591)

--- source/syntax/SyntaxVisitor.cpp
+++ source/syntax/SyntaxVisitor.cpp
@@ -12,13 +12,10 @@ using namespace slang;
 
 struct CloneVisitor {
     BumpAllocator& alloc;
-    const slang::detail::ChangeMap& changes;
-    const slang::detail::ListChangeMap& listAdditions;
+    const slang::detail::ChangeCollection& commits;
 
-    CloneVisitor(BumpAllocator& alloc, const slang::detail::ChangeMap& changes,
-                 const slang::detail::ListChangeMap& listAdditions) :
-        alloc(alloc),
-        changes(changes), listAdditions(listAdditions) {}
+    CloneVisitor(BumpAllocator& alloc, const slang::detail::ChangeCollection& commits) :
+        alloc(alloc), commits(commits) {}
 
 #ifdef _MSC_VER
 #    pragma warning(push)
@@ -29,41 +26,30 @@ struct CloneVisitor {
         T* cloned = node.clone(alloc);
 
         constexpr bool IsList = std::is_same_v<T, SyntaxListBase>;
-        optional<SmallVectorSized<TokenOrSyntax, 8>> listBuffer;
+        SmallVectorSized<TokenOrSyntax, 8> listBuffer;
 
         if constexpr (IsList) {
-            if (auto it = listAdditions.find(&node); it != listAdditions.end()) {
-                listBuffer.emplace();
+            if (auto it = commits.listInsertAtFront.find(&node);
+                it != commits.listInsertAtFront.end()) {
 
                 const slang::detail::SyntaxChange* lastChange = nullptr;
-                for (auto& change : it->second) {
-                    if (change.kind == slang::detail::SyntaxChange::InsertAtFront) {
-                        if (!listBuffer->empty() && change.separator)
-                            listBuffer->append(change.separator);
-                        listBuffer->append(change.second);
-                        lastChange = &change;
-                    }
+                for (const auto& change : it->second) {
+                    if (!listBuffer.empty() && change.separator)
+                        listBuffer.append(change.separator);
+                    listBuffer.append(change.second);
+                    lastChange = &change;
                 }
 
                 if (lastChange && node.getChildCount() && lastChange->separator)
-                    listBuffer->append(lastChange->separator);
+                    listBuffer.append(lastChange->separator);
             }
         }
-
-        auto backfillList = [&](size_t index) {
-            if (cloned->kind != SyntaxKind::SyntaxList && cloned->kind != SyntaxKind::SeparatedList)
-                throw std::logic_error("Can't use insertBefore or insertAfter on a non-list node");
-
-            listBuffer.emplace();
-            for (size_t i = 0; i < index; i++)
-                listBuffer->append(cloned->getChild(i));
-        };
 
         for (size_t i = 0; i < node.getChildCount(); i++) {
             auto child = node.childNode(i);
             if (!child) {
-                if (IsList && listBuffer)
-                    listBuffer->append(node.childToken(i));
+                if constexpr (IsList)
+                    listBuffer.append(node.childToken(i));
                 continue;
             }
 
@@ -71,62 +57,57 @@ struct CloneVisitor {
             // want to insert or remove elements from it. Once we see the first
             // modification we start building the buffer instead, and then replace
             // the whole list in one go at the end.
-            auto it = changes.find(child);
-            if (it == changes.end()) {
-                if (IsList && listBuffer)
-                    listBuffer->append(child->visit(*this));
-                else
-                    cloned->setChild(i, child->visit(*this));
+            if (auto it = commits.insertBefore.find(child); it != commits.insertBefore.end()) {
+                if (cloned->kind != SyntaxKind::SyntaxList &&
+                    cloned->kind != SyntaxKind::SeparatedList)
+                    throw std::logic_error(
+                        "Can't use insertBefore or insertAfter on a non-list node");
+                for (const auto& change : it->second)
+                    listBuffer.append(change.second);
+            }
+
+            if (auto it = commits.removeOrReplace.find(child);
+                it != commits.removeOrReplace.end()) {
+                if (auto replaceChange = std::get_if<detail::ReplaceChange>(&it->second);
+                    replaceChange) {
+                    if constexpr (IsList)
+                        listBuffer.append(replaceChange->second);
+                    else
+                        cloned->setChild(i, replaceChange->second);
+                }
             }
             else {
-                switch (it->second.kind) {
-                    case slang::detail::SyntaxChange::Remove:
-                        THROW_UNREACHABLE; // TODO: implement this
-
-                    case slang::detail::SyntaxChange::Replace:
-                        if (IsList && listBuffer)
-                            listBuffer->append(it->second.second);
-                        else
-                            cloned->setChild(i, it->second.second);
-                        break;
-                    case slang::detail::SyntaxChange::InsertBefore:
-                        if (!listBuffer)
-                            backfillList(i);
-                        listBuffer->append(it->second.second);
-                        listBuffer->append(child->visit(*this));
-                        break;
-                    case slang::detail::SyntaxChange::InsertAfter:
-                        if (!listBuffer)
-                            backfillList(i);
-                        listBuffer->append(child->visit(*this));
-                        listBuffer->append(it->second.second);
-                        break;
-                    case slang::detail::SyntaxChange::InsertAtFront:
-                    case slang::detail::SyntaxChange::InsertAtBack:
-                    default:
-                        THROW_UNREACHABLE;
+                if constexpr (IsList) {
+                    listBuffer.append(child->visit(*this));
                 }
+                else {
+                    cloned->setChild(i, child->visit(*this));
+                    listBuffer.append(cloned->getChild(i));
+                }
+            }
+
+            if (auto it = commits.insertAfter.find(child); it != commits.insertAfter.end()) {
+                if (cloned->kind != SyntaxKind::SyntaxList &&
+                    cloned->kind != SyntaxKind::SeparatedList)
+                    throw std::logic_error(
+                        "Can't use insertBefore or insertAfter on a non-list node");
+                for (const auto& change : it->second)
+                    listBuffer.append(change.second);
             }
         }
 
         if constexpr (IsList) {
-            if (auto it = listAdditions.find(&node); it != listAdditions.end()) {
-                if (!listBuffer)
-                    backfillList(node.getChildCount());
+            if (auto it = commits.listInsertAtBack.find(&node);
+                it != commits.listInsertAtBack.end()) {
 
-                for (auto& change : it->second) {
-                    if (change.kind == slang::detail::SyntaxChange::InsertAtBack) {
-                        if (!listBuffer->empty() && change.separator)
-                            listBuffer->append(change.separator);
-                        listBuffer->append(change.second);
-                    }
+                for (const auto& change : it->second) {
+                    if (!listBuffer.empty() && change.separator)
+                        listBuffer.append(change.separator);
+                    listBuffer.append(change.second);
                 }
             }
 
-            if (listBuffer) {
-                cloned->resetAll(alloc, *listBuffer);
-                listBuffer.reset();
-            }
+            cloned->resetAll(alloc, listBuffer);
         }
 
         return cloned;
@@ -145,10 +126,10 @@ struct CloneVisitor {
 namespace slang::detail {
 
 std::shared_ptr<SyntaxTree> transformTree(
-    BumpAllocator&& alloc, const std::shared_ptr<SyntaxTree>& tree, const ChangeMap& changes,
-    const ListChangeMap& listAdditions, const std::vector<std::shared_ptr<SyntaxTree>>& tempTrees) {
+    BumpAllocator&& alloc, const std::shared_ptr<SyntaxTree>& tree, const ChangeCollection& commits,
+    const std::vector<std::shared_ptr<SyntaxTree>>& tempTrees) {
 
-    CloneVisitor visitor(alloc, changes, listAdditions);
+    CloneVisitor visitor(alloc, commits);
     SyntaxNode* root = tree->root().visit(visitor);
 
     // Steal ownership of any temporary syntax trees that the user created; once we return the
