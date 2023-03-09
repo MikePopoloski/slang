eb3f8cdce12a145971891490d65615f97a94bf87
Jamie Hanlon <jamieh@graphcore.ai>
2023-03-09 20:44:10 +0000

    WIP

--- tools/netlist/include/DirectedGraph.h
+++ tools/netlist/include/DirectedGraph.h
@@ -12,11 +12,22 @@
 #include <vector>
 #include <cassert>
 
+// Inspired by LLVM's DirectedGraph ADT.
+// https://llvm.org/doxygen/DirectedGraph_8h_source.html
+
 /// A class to represent a directed edge in the graph.
 template<class NodeType, class EdgeType>
 class DirectedEdge {
 public:
   DirectedEdge(NodeType &targetNode) : targetNode(targetNode) {}
+
+  DirectedEdge<NodeType, EdgeType> &operator=(const DirectedEdge<NodeType, EdgeType> &edge) {
+    targetNode = edge.targetNode;
+    return *this;
+  }
+
+  const NodeType &getTargetNode() { return targetNode; }
+
 protected:
   NodeType &targetNode;
 };
@@ -28,6 +39,7 @@ public:
   using EdgeListType = std::vector<std::unique_ptr<EdgeType>>;
   using iterator = typename EdgeListType::iterator;
   using const_iterator = typename EdgeListType::const_iterator;
+  using edge_descriptor = EdgeType*;
 
   Node() = default;
   Node(EdgeType &edge) : edges() { edges.push_back(edge); }
@@ -37,6 +49,20 @@ public:
   iterator begin() { return edges.begin(); }
   iterator end() { return edges.end(); }
 
+  Node<NodeType, EdgeType> &operator=(const Node<NodeType, EdgeType> &node) {
+    edges = node.edges;
+    return *this;
+  }
+  Node<NodeType, EdgeType> &operator=(const Node<NodeType, EdgeType> &&node) {
+    edges = std::move(node.Edges);
+    return *this;
+  }
+
+  /// Static polymorphism: delegate implementation (via isEqualTo) to the
+  /// derived class.
+  bool operator==(const NodeType &N) const { return getDerived().isEqualTo(N); }
+  bool operator!=(const NodeType &N) const { return !operator==(N); }
+
   /// Add an edge between this node and a target node.
   EdgeType *addEdge(NodeType &targetNode) {
     auto edge = std::make_unique<EdgeType>(targetNode);
@@ -44,13 +70,38 @@ public:
     return edges.back().get();
   }
 
-  //EdgeType getEdge()
+  /// Populate a result vector of edges from this node to the specified target
+  /// node. Return true if at least one edge was found.
+  bool getEdgesTo(const NodeType &targetNode, std::vector<EdgeType*> &result) {
+    assert(result.empty() && "Expected the results parameter to be empty");
+    for (auto &edge : edges) {
+      if (edge->getTargetNode() == targetNode) {
+        result.push_back(edge.get());
+      }
+    }
+    return !result.empty();
+  }
 
-  //void removeEdge(EdgeType &edge) { edges.remove(&edge); }
+  void removeEdge(EdgeType &edge) { edges.remove(&edge); }
+
+  const EdgeListType &getEdges() const { return edges; }
+
+  size_t outDegree() const { return edges.size(); }
 
   void clearEdges() { edges.clear(); }
 
-private:
+protected:
+  // As the default implementation use address comparison for equality.
+  bool isEqualTo(const NodeType &N) const {
+    return this == &N;
+  }
+  NodeType &getDerived() {
+    return *static_cast<NodeType*>(this);
+  }
+  const NodeType &getDerived() const {
+    return *static_cast<const NodeType*>(this);
+  }
+
   EdgeListType edges;
 };
 
@@ -68,12 +119,6 @@ public:
 
   DirectedGraph() = default;
 
-  // Find a given node.
-  //const_iterator findNode(const NodeType &nodeToFind) const {
-  //  return std::find_if(nodes,
-  //                      [&nodeToFind](const NodeType *node) { return *node == nodeToFind; });
-  //}
-
   /// Add a node to the graph and return a descriptor.
   node_descriptor addNode() {
     nodes.push_back(std::make_unique<NodeType>());
@@ -90,7 +135,26 @@ public:
   /// Retrieve a node by its descriptor.
   NodeType &getNode(node_descriptor node) {
     assert(node < nodes.size() && "Node does not exist");
-    return nodes[node];
+    return *nodes[node];
+  }
+
+  /// Collect all edges that
+  bool getInEdgesToNode(node_descriptor nodeDesc, std::vector<EdgeType*> &results) const {
+    assert(results.empty() && "Expected the results parameter to be empty");
+    std::vector<EdgeType*> tempResults;
+    for (auto &node : nodes) {
+      node->getEdgesTo(*(nodes[nodeDesc].get()), tempResults);
+      results.insert(results.end(), tempResults.begin(), tempResults.end());
+      tempResults.clear();
+    }
+    return !results.empty();
+  }
+
+  /// Return the number of edges incident on the specified node.
+  size_t inDegree(node_descriptor nodeDesc) {
+    std::vector<EdgeType*> results;
+    getInEdgesToNode(nodeDesc, results);
+    return results.size();
   }
 
   /// Return the size of the graph.
--- tools/netlist/tests/DirectedGraphTest.cpp
+++ tools/netlist/tests/DirectedGraphTest.cpp
@@ -8,7 +8,7 @@
 #include "Test.h"
 #include "DirectedGraph.h"
 
-TEST_CASE("Basic connectivity test") {
+TEST_CASE("Basic construction test") {
 
   struct TestNode;
   struct TestEdge;
@@ -25,8 +25,23 @@ TEST_CASE("Basic connectivity test") {
   auto n0 = graph.addNode();
   auto n1 = graph.addNode();
   auto n2 = graph.addNode();
+  auto n3 = graph.addNode();
   graph.addEdge(n0, n1);
   graph.addEdge(n0, n2);
-  CHECK(true);
+  graph.addEdge(n0, n3);
+  graph.addEdge(n1, n2);
+  graph.addEdge(n1, n3);
+  graph.addEdge(n2, n3);
+  CHECK(graph.size() == 4);
+  // Out edges.
+  CHECK(graph.getNode(n0).outDegree() == 3);
+  CHECK(graph.getNode(n1).outDegree() == 2);
+  CHECK(graph.getNode(n2).outDegree() == 1);
+  CHECK(graph.getNode(n3).outDegree() == 0);
+  // In edges.
+  CHECK(graph.inDegree(n0) == 0);
+  CHECK(graph.inDegree(n1) == 1);
+  CHECK(graph.inDegree(n2) == 2);
+  CHECK(graph.inDegree(n3) == 3);
 }
 
