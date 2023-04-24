149d98ae0156c58f386bf3485a870c51d82b3e8a
James Hanlon <mail@jameswhanlon.com>
2023-04-24 20:40:42 +0100

    DFS WIP

--- tools/netlist/include/DepthFirstSearch.h
+++ tools/netlist/include/DepthFirstSearch.h
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-//! @file DepthFirstSearch.h
+//! @file DepthFirstIterator.h
 //! @brief Implementation of depth-first search for a directed graph.
 //
 // SPDX-FileCopyrightText: Michael Popoloski
@@ -13,24 +13,81 @@
 #include "DirectedGraph.h"
 
 template<class NodeType, class EdgeType>
-class DepthFirstSearch {
+class DepthFirstIterator {
 public:
-  DepthFirstSearch(DirectedGraph<NodeType, EdgeType> &graph) : graph(graph) {}
+  using GraphType = DirectedGraph<NodeType, EdgeType>;
+  using iterator_category = std::forward_iterator_tag;
+  using difference_type = std::ptrdiff_t;
+  using value_type = NodeType;
+  using pointer = NodeType*;
+  using reference = NodeType&;
 
-  /// Perform a depth-first search, terminating when targetNode is reached.
-  void search(NodeType &sourceNode, NodeType &targetNode) {
-    //visitedNodes.insert(sourceNode);
-    //visitStack.push_back(sourceNode);
-    //for (const auto it = sourceNode.begin(); it != sourceNode.end(); it++) {
-    //  const auto targetNode = it.second.getTargetNode();
-    //  if (visitedNodes.count(targetNode) == 0) {
-    //    visitStack.push_back(targetNode);
-    //  }
-    //}
+  DepthFirstIterator(const NodeType &startNode) {
+    visitedNodes.insert(startNode);
+    visitStack.push_back(StackElement(startNode, startNode.begin()));
   }
 
+  DepthFirstIterator() = default;
+
 private:
-  DirectedGraph<NodeType, EdgeType> &graph;
+  using EdgeIteratorType = typename GraphType::iterator;
+  using VisitStackElement = std::pair<NodeType&, EdgeIteratorType>;
+
+  /// Perform a depth-first search, terminating when targetNode is reached.
+  void next() {
+    do {
+      // Take the top node on the visit stack.
+      auto &node = visitStack.back().first;
+      auto &nodeIt = visitStack.back().second;
+      // Visit each child node that hasn't already been visited.
+      while (nodeIt != node.end()) {
+        const auto targetNode = nodeIt.second.getTargetNode();
+        if (visitedNodes.count(targetNode) == 0) {
+          // Push a new 'current' node onto the stack.
+          visitStack.push_back(VisitStackElement(targetNode, targetNode.begin()));
+          return;
+        }
+        nodeIt++;
+      }
+      // Visited all children of this node.
+      visitStack.pop_back();
+    } while (!visitStack.empty());
+  }
+
+public:
+  static DepthFirstIterator begin(const NodeType &startNode) {
+    return DepthFirstIterator(startNode);
+  }
+  static DepthFirstIterator end() {
+    return DepthFirstIterator();
+  }
+
+  reference operator*() const { return visitStack.back(); }
+
+  pointer operator->() { return &visitStack.back(); }
+
+  /// Prefix increment
+  DepthFirstIterator& operator++() {
+    next();
+    return *this;
+  }
+
+  /// Postfix increment
+  DepthFirstIterator operator++(int) {
+    DepthFirstIterator tmp = *this;
+    ++(*this);
+    return tmp;
+  }
+
+  friend bool operator== (const DepthFirstIterator& a, const DepthFirstIterator& b) {
+    return a.visitStack == b.visitStack;
+  };
+
+  friend bool operator!= (const DepthFirstIterator& a, const DepthFirstIterator& b) {
+    return a.visitStack != b.visitStack;;
+  };
+
+private:
   std::set<NodeType&> visitedNodes;
   std::vector<NodeType&> visitStack;
 };
