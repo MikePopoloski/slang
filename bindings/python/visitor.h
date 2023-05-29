5aa870301168165dba1e7f1dab6b2541a01a9314
Thomas Peters <thomas.d.peters@gmail.com>
2023-05-29 19:36:49 -0400

    Python bindings: expose a visitor pattern (#771)
    * pyslang.h: correct file reference in leading comment
    
    * Python bindings: expose a visitor pattern
    
    Expose visit methods on pyslang Expressions, Statements, and Symbols.
    This method is modelled after MLIR's visitor pattern wherein
    callbacks can signal certain actions on the walk, such as
    interrupting (i.e., stopping entirely), skipping child nodes,
    or to advance.
    
    This commit includes no tests, as pyslang tests are not included
    in the slang repo, but in the pyslang repo. Once this commit
    is visible to pyslang, tests will be added there.
    
    Here is an example test case that could be added, modelled off
    slang/tests/unittests/VisitorTests.cpp:
    
    ```
    from pyslang import *
    
    tree = SyntaxTree.fromText(
      """
    module m;
      int j;
      initial begin : asdf
        j = j + 3;
        if (1) begin : baz
          static int i;
          i = i + 2;
          if (1) begin : boz
            i = i + 4
          end
        end
      end
    endmodule
    """)
    
    class Visitor:
      def __init__(self):
        self.stmt_ct = 0
      def visit(self, node):
        if isinstance(node, Statement):
          self.stmt_ct += 1
    
    c = Compilation()
    c.addSyntaxTree(tree)
    v = Visitor()
    c.getRoot().visit(v.visit)
    assert v.stmt_ct == 11
    ```

---
+++ bindings/python/visitor.h
@@ -0,0 +1,50 @@
+//------------------------------------------------------------------------------
+// visitor.h
+// SPDX-FileCopyrightText: Michael Popoloski
+// SPDX-License-Identifier: MIT
+//------------------------------------------------------------------------------
+#pragma once
+
+#include <pybind11/pybind11.h>
+
+#include "slang/ast/ASTVisitor.h"
+
+enum class VisitAction {
+    Advance,
+    Skip,
+    Interrupt,
+};
+
+struct PyVisitor : ASTVisitor<PyVisitor, true, true> {
+    py::object f;
+    bool interrupted = false;
+
+    static inline constexpr auto doc =
+        "Visit a pyslang object with a callback f.\n\n"
+        "If f ever returns pyslang.VisitAction.Interrupt, the visit is aborted, "
+        "and no additional nodes are visited. If f returns pyslang.VisitAction.Skip, "
+        "then no child nodes of the current node are visited, but otherwise the "
+        "visit continues. Any other return value, including "
+        "pslang.VisitAction.Advance is ignored, and the walk continues.";
+
+    explicit PyVisitor(py::object f) : f{f} {}
+
+    template<typename T>
+    void handle(const T& t) {
+        if (interrupted)
+            return;
+        py::object result = f(&t);
+        if (result.equal(py::cast(VisitAction::Interrupt))) {
+            interrupted = true;
+            return;
+        }
+        if (result.not_equal(py::cast(VisitAction::Skip)))
+            visitDefault(t);
+    }
+};
+
+template<typename T>
+void pyVisit(const T& t, py::object f) {
+    PyVisitor visitor{f};
+    t.visit(visitor);
+}
