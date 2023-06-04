72bbb091a38fd36c50a6cf3dd86a0122bd635ac2
MikePopoloski <mike@popoloski.com>
2023-06-03 21:26:37 -0400

    Support using checkers with bind directives

--- include/slang/ast/ASTContext.h
+++ include/slang/ast/ASTContext.h
@@ -361,7 +361,7 @@ public:
     /// Clears the parent instance and parent procedural block symbol
     /// associated with the context.
     void clearInstanceAndProc() { instanceOrProc = nullptr; }
-    const CheckerInstanceBodySymbol* tryFillAssertionDetails();
+    const Symbol* tryFillAssertionDetails();
 
     /// Tries to fill the @a assertionInstance member by searching upward through
     /// parent scopes to find an assertion instantiation.
