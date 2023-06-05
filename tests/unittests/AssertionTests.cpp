157401ee2d93396a7bf9dc5485af94d3589e9d55
MikePopoloski <mike@popoloski.com>
2023-06-05 11:12:49 -0400

    Add test for inferred value context functions in checker args

--- tests/unittests/AssertionTests.cpp
+++ tests/unittests/AssertionTests.cpp
@@ -1856,3 +1856,39 @@ endchecker
     REQUIRE(diags.size() == 1);
     CHECK(diags[0].code == diag::NotAllowedInChecker);
 }
+
+TEST_CASE("Inferred value sys funcs in checkers") {
+    auto tree = SyntaxTree::fromText(R"(
+checker check_in_context (logic test_sig,
+                          event clock = $inferred_clock,
+                          logic reset = $inferred_disable);
+    property p(logic sig); 1; endproperty
+    a1: assert property (@clock disable iff (reset) p(test_sig));
+    c1: cover property (@clock !reset throughout !test_sig ##1 test_sig);
+endchecker : check_in_context
+
+module m(logic rst);
+    wire clk;
+    logic a, en;
+    wire b = a && en;
+
+    // No context inference
+    check_in_context my_check1(.test_sig(b), .clock(clk), .reset(rst));
+
+    always @(posedge clk) begin
+        a <= 1;
+        if (en) begin
+            // inferred from context:
+            // .clock(posedge clk)
+            // .reset(1'b0)
+            check_in_context my_check2(a);
+        end
+        en <= 1;
+    end
+endmodule : m
+)");
+
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+    NO_COMPILATION_ERRORS;
+}
