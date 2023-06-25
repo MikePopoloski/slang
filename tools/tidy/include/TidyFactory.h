176c9728887e5b568adfd7804e707b8927282de6
Josep Sans i Prats <josepsansprats@gmail.com>
2023-06-25 14:22:07 +0200

    slang-tidy trying to fix win64 linking issues

--- tools/tidy/include/TidyFactory.h
+++ tools/tidy/include/TidyFactory.h
@@ -21,7 +21,7 @@
 
 class TidyCheck;
 
-class Registry {
+class SLANG_EXPORT Registry {
 public:
     using RegistryFunction = std::function<std::unique_ptr<TidyCheck>()>;
     struct RegistryValue {
@@ -69,11 +69,11 @@ public:
     static const TidyConfig& getConfig() { return config(); }
 
     static void setSourceManager(const slang::SourceManager* sm) { *sourceManager() = sm; }
-    static const slang::SourceManager* getSourceManager() {
+    static slang::not_null<const slang::SourceManager*> getSourceManager() {
         if (auto sm = *sourceManager(); sm == nullptr)
             SLANG_THROW(std::runtime_error("TidyFactory: Trying to get SourceManager, but factory "
                                            "pointer has not been initialized"));
-        return *sourceManager();
+        return slang::not_null<const slang::SourceManager*>(*sourceManager());
     }
 
 private:
