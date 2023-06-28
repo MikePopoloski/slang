20e4e1c344ade9f5e8094132f81b749476658a56
MikePopoloski <mike@popoloski.com>
2023-06-28 05:32:41 -0400

    Merge CppTypePrinter into TypeTraits header

--- include/slang/util/TypeTraits.h
+++ include/slang/util/TypeTraits.h
@@ -9,7 +9,45 @@
 
 #include <type_traits>
 
+#include "slang/util/Util.h"
+
 namespace slang {
+
+/// Returns the demangled name of the template argument's C++ type.
+template<typename T>
+constexpr std::string_view typeName();
+
+template<>
+constexpr std::string_view typeName<void>() {
+    return "void";
+}
+
+namespace detail {
+
+template<typename T>
+constexpr std::string_view wrappedTypeName() {
+    return SLANG_ASSERT_FUNCTION;
+}
+
+constexpr std::size_t wrappedTypeNamePrefixLength() {
+    return wrappedTypeName<void>().find(typeName<void>());
+}
+
+constexpr std::size_t wrappedTypeNameSuffixLength() {
+    return wrappedTypeName<void>().length() - wrappedTypeNamePrefixLength() -
+           typeName<void>().length();
+}
+
+} // namespace detail
+
+template<typename T>
+constexpr std::string_view typeName() {
+    // https://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c/64490578#64490578
+    auto name = detail::wrappedTypeName<T>();
+    auto prefixLength = detail::wrappedTypeNamePrefixLength();
+    auto suffixLength = detail::wrappedTypeNameSuffixLength();
+    return name.substr(prefixLength, name.length() - prefixLength - suffixLength);
+}
 
 /// A simple implementation of a type index that can stand in for std::type_index
 /// to allow building without RTTI enabled.
