d2e45dcd53eb3cc89b340a9c1189cbe854c99d61
James Hanlon <mail@jameswhanlon.com>
2023-06-30 08:35:05 +0100

    Add tests and disclaimer

--- tools/netlist/tests/NetlistTests.cpp
+++ tools/netlist/tests/NetlistTests.cpp
@@ -410,3 +410,60 @@ endmodule
     CHECK(pathFinder.find(*inPortA, *outPortB).empty());
     CHECK(pathFinder.find(*inPortB, *outPortA).empty());
 }
+
+//===---------------------------------------------------------------------===//
+// Tests for conditional variables in procedural blocks.
+//===---------------------------------------------------------------------===//
+
+TEST_CASE("Mux") {
+    // Test that the variable in a conditional block is correctly added as a
+    // dependency on the output variable controlled by that block.
+    auto tree = SyntaxTree::fromText(R"(
+module mux(input a, input b, input sel, output reg f);
+  always @(*) begin
+    if (sel == 1'b0) begin
+      f = a;
+    end else begin
+      f = b;
+    end
+  end
+endmodule
+)");
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+    NO_COMPILATION_ERRORS;
+    auto netlist = createNetlist(compilation);
+    PathFinder pathFinder(netlist);
+    CHECK(!pathFinder.find(*netlist.lookupPort("mux.sel"),
+                           *netlist.lookupPort("mux.f")).empty());
+}
+
+TEST_CASE("Nested muxing") {
+    // Test that the variables in multiple nested levels of conditions are
+    // correctly added as dependencies of the output variable.
+    auto tree = SyntaxTree::fromText(R"(
+module mux(input a, input b, input c,
+           input sel_a, input sel_b,
+           output reg f);
+  always @(*) begin
+    if (sel_a == 1'b0) begin
+      if (sel_b == 1'b0)
+        f = a;
+      else
+        f = b;
+    end else begin
+      f = c;
+    end
+  end
+endmodule
+)");
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+    NO_COMPILATION_ERRORS;
+    auto netlist = createNetlist(compilation);
+    PathFinder pathFinder(netlist);
+    CHECK(!pathFinder.find(*netlist.lookupPort("mux.sel_a"),
+                           *netlist.lookupPort("mux.f")).empty());
+    CHECK(!pathFinder.find(*netlist.lookupPort("mux.sel_b"),
+                           *netlist.lookupPort("mux.f")).empty());
+}
