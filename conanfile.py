e869344fe69d72b004b2d0186a599ed005254bff
MikePopoloski <mike@popoloski.com>
2023-12-31 21:30:29 -0500

    Bump Catch2 and pybind11 dependency versions

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,8 @@ class CompressorRecipe(ConanFile):
 
     def requirements(self):
         self.requires("mimalloc/2.1.2")
-        self.requires("catch2/3.4.0")
-        self.requires("pybind11/2.10.4")
+        self.requires("catch2/3.5.1")
+        self.requires("pybind11/2.11.1")
         self.requires("fmt/10.1.1")
 
     def layout(self):
