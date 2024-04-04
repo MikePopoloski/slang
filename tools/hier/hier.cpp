c4c3e329f95881423f7168a056c65f540a3a88d9
Udi Finkelstein <github@udifink.com>
2024-04-05 01:27:45 +0300

    Display a hierarchical list of module and instance names

---
+++ tools/hier/hier.cpp
@@ -0,0 +1,69 @@
+#include "slang/ast/Compilation.h"
+#include "slang/ast/ASTVisitor.h"
+#include "slang/ast/symbols/InstanceSymbols.h"
+#include "slang/driver/Driver.h"
+#include "slang/util/Version.h"
+
+using namespace slang;
+using namespace slang::driver;
+using namespace slang::ast;
+
+int main(int argc, char** argv) {
+    Driver driver;
+    driver.addStandardArgs();
+
+    std::optional<bool> showHelp;
+    std::optional<bool> showVersion;
+    driver.cmdLine.add("-h,--help", showHelp, "Display available options");
+    driver.cmdLine.add("--version", showVersion, "Display version information and exit");
+
+    if (!driver.parseCommandLine(argc, argv))
+        return 1;
+
+    if (showHelp == true) {
+        printf("%s\n", driver.cmdLine.getHelpText("slang SystemVerilog compiler").c_str());
+        return 0;
+    }
+
+    if (showVersion == true) {
+        printf("slang version %d.%d.%d+%s\n", VersionInfo::getMajor(),
+            VersionInfo::getMinor(), VersionInfo::getPatch(),
+            std::string(VersionInfo::getHash()).c_str());
+        return 0;
+    }
+
+    if (!driver.processOptions())
+        return 2;
+
+    bool ok = driver.parseAllSources();
+
+    auto compilation = driver.createCompilation();
+
+    std::string path;
+    auto instances = compilation->getRoot().topInstances;
+    for (auto& i: instances)
+        i->visit(makeVisitor([&](auto& visitor, const InstanceSymbol& type) {
+            if (type.isModule()) {
+                std::string tmp_path;
+                //std::string tmp_path2;
+                type.getHierarchicalPath(tmp_path);
+                //type.getLexicalPath(tmp_path2);
+                //static std::string path;
+                //int len = path.length();
+                //path.reserve(len + 1 + type.name.size());  // Reserve enough space
+                //if (len)
+                //    path.append(".");
+                //path.append(type.name);
+                //OS::print(fmt::fg(fmt::color::yellow_green),
+                //            fmt::format("Module: {} Instance: {}\n", type.getDefinition().name, path));
+                OS::print(fmt::fg(fmt::color::yellow_green),
+                            fmt::format("Instance (Module): {} ({})\n", tmp_path, type.getDefinition().name));
+                visitor.visitDefault(type);
+                //path.resize(len);
+            }
+        }));
+
+    ok &= driver.reportCompilation(*compilation, /* quiet */ false);
+
+    return ok ? 0 : 3;
+}
