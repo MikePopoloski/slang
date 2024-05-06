MikePopoloski/slang

commit 4f37beaa243fb63f2731f70a44d8570bc6096ee3

Slang-netlist: fix handling of generate blocks (#986)
Fix #985

tools/netlist/include/NetlistVisitor.h
@@ -246,6 +246,10 @@ public:
         evalCtx.pushEmptyFrame();
     }
 
+    void handle(const ast::VariableSymbol& symbol) { netlist.addVariableDeclaration(symbol); }
+
+    void handle(const ast::NetSymbol& symbol) { netlist.addVariableDeclaration(symbol); }
+
     void handle(const ast::ForLoopStatement& loop) {
 
         // Conditions this loop cannot be unrolled.
@@ -539,11 +543,8 @@ public:
         }
     }
 
-    /// Handle making connections form port members to internal variable
-    /// declarations. This must be called before 'handleInstanceExtPortConn'
-    /// becuase it creates the port declarations in the netlist that are
-    /// connected to externally.
-    auto handleInstanceIntVars(ast::InstanceSymbol const& symbol) {
+    /// Create instance variable declarations.
+    auto handleInstanceMemberVars(ast::InstanceSymbol const& symbol) {
 
         for (auto& member : symbol.body.members()) {
             if (member.kind == ast::SymbolKind::Variable || member.kind == ast::SymbolKind::Net) {
@@ -552,11 +553,10 @@ public:
         }
     }
 
-    /// Handle making connections form port members to internal variable
-    /// declarations. This must be called before 'handleInstanceExtPortConn'
-    /// becuase it creates the port declarations in the netlist that are
-    /// connected to externally.
-    auto handleInstanceIntPorts(ast::InstanceSymbol const& symbol) {
+    /// Create instance port declarations. Must be called after
+    /// handleInstanceMemberVars() in order that ports can be connected to
+    /// their corresponding variables.
+    auto handleInstanceMemberPorts(ast::InstanceSymbol const& symbol) {
 
         for (auto& member : symbol.body.members()) {
             if (member.kind == ast::SymbolKind::Port) {
@@ -568,13 +568,13 @@ public:
         }
     }
 
-    /// Variable declaration.
+    /// Variable declaration (deferred to handleInstanceMemberVars).
     void handle(const ast::VariableSymbol& symbol) {}
 
-    /// Net declaration.
+    /// Net declaration (deferred to handleInstanceMemberVars).
     void handle(const ast::NetSymbol& symbol) {}
 
-    /// Port declaration.
+    /// Port declaration (deferred to handleInstanceMemberPorts).
     void handle(const ast::PortSymbol& symbol) {}
 
     /// Instance.
@@ -588,8 +588,8 @@ public:
             return;
         }
 
-        handleInstanceIntVars(symbol);
-        handleInstanceIntPorts(symbol);
+        handleInstanceMemberVars(symbol);
+        handleInstanceMemberPorts(symbol);
         handleInstanceExtPorts(symbol);
 
         symbol.body.visit(*this);
@@ -599,6 +599,14 @@ public:
     void handle(const ast::ProceduralBlockSymbol& symbol) {
         ProceduralBlockVisitor visitor(compilation, netlist);
         symbol.visit(visitor);
+    }
+
+    /// Generate block.
+    void handle(const ast::GenerateBlockSymbol& symbol) {
+        if (!symbol.isUninstantiated) {
+            ProceduralBlockVisitor visitor(compilation, netlist);
+            symbol.visit(visitor);
+        }
     }
 
     /// Continuous assignment statement.

tools/netlist/tests/PathTests.cpp
@@ -619,3 +619,29 @@ endmodule
     PathFinder pathFinder(netlist);
     CHECK(!pathFinder.find(*inPort, *outPort).empty());
 }
+
+//===---------------------------------------------------------------------===//
+// Test case for for #985 (generate blocks)
+//===---------------------------------------------------------------------===//
+
+TEST_CASE("Conditional generate blocks") {
+    // One branch of the generate conditional is uninstantiated.
+    auto tree = SyntaxTree::fromText(R"(
+module top#(parameter X=0)(output logic out);
+generate
+if (X) begin
+  logic foo;
+  assign out = foo;
+end else begin
+  logic foo;
+  assign out = foo;
+end
+endgenerate
+endmodule
+)");
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+    NO_COMPILATION_ERRORS;
+    auto netlist = createNetlist(compilation);
+    CHECK(netlist.lookupVariable("top.genblk1.foo"));
+}

commit e3933161a0f89a763cfe2e5809987ca2c3325443

Fix breakage caused by https://github.com/MikePopoloski/slang/pull/986

tools/netlist/include/NetlistVisitor.h
@@ -622,7 +622,7 @@ public:
     /// Generate block.
     void handle(const ast::GenerateBlockSymbol& symbol) {
         if (!symbol.isUninstantiated) {
-            ProceduralBlockVisitor visitor(compilation, netlist);
+            ProceduralBlockVisitor visitor(compilation, netlist, ast::EdgeKind::None);
             symbol.visit(visitor);
         }
     }