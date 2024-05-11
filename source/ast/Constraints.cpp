7ef6d36859fe0374d08543ad8015d9be52277530
MikePopoloski <mike@popoloski.com>
2024-05-10 21:24:22 -0400

    v1800-2023 clarification: functions in constraints cannot have inout args

--- source/ast/Constraints.cpp
+++ source/ast/Constraints.cpp
@@ -225,6 +225,7 @@ struct ConstraintExprVisitor {
                         const SubroutineSymbol& sub = *std::get<0>(call.subroutine);
                         for (auto arg : sub.getArguments()) {
                             if (arg->direction == ArgumentDirection::Out ||
+                                arg->direction == ArgumentDirection::InOut ||
                                 (arg->direction == ArgumentDirection::Ref &&
                                  !arg->flags.has(VariableFlags::Const))) {
                                 context.addDiag(diag::OutRefFuncConstraint, expr.sourceRange);
