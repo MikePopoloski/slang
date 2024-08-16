697ed082b5e4fdc58c8f5a4d13bafcf2cc38ab9b
Yan Churkin <yan@ispras.ru>
2024-08-16 20:06:29 +0300

    Fix flag propagation

--- source/ast/expressions/MiscExpressions.cpp
+++ source/ast/expressions/MiscExpressions.cpp
@@ -987,7 +987,8 @@ Expression& AssertionInstanceExpression::fromLookup(const Symbol& symbol,
     ASTContext bodyContext(*symbolScope, LookupLocation::max);
     bodyContext.assertionInstance = &instance;
     // Propagate previously founded time advance specs
-    bodyContext.flags |= ASTFlags::PropertyTimeAdvance;
+    if (context.flags.has(ASTFlags::PropertyTimeAdvance))
+        bodyContext.flags |= ASTFlags::PropertyTimeAdvance;
 
     // Let declarations expand directly to an expression.
     if (symbol.kind == SymbolKind::LetDecl)
