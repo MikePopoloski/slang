09a397c4434d83697ad9902d97cdf64bb90a4a29
Sebastiano Pomata <sebastiano.pomata@semidynamics.com>
2025-01-10 16:49:26 +0100

    clock name regex string dedicated tests
    - adding valid/invalid case for clock regex matching
    - updating README with default config value/example

--- tools/tidy/tests/UnusedSensitiveSignalTest.cpp
+++ tools/tidy/tests/UnusedSensitiveSignalTest.cpp
@@ -114,3 +114,77 @@ endmodule
     bool result = visitor->check(root);
     CHECK(result);
 }
+
+TEST_CASE("UnusedSensitiveSignal: Clock signals not matching regex expression will raise a warning") {
+    auto tree = SyntaxTree::fromText(R"(
+module d_ff_a_en
+(
+    input logic ck_a_i, rst_i, en_i, c_i, d_i,
+
+    output logic q_o
+) ;
+always_ff @ (posedge ck_a_i, posedge rst_i) begin
+    if (rst_i)
+        q_o <= '0;
+    else if (en_i)
+        q_o <= d_i;
+    end
+endmodule
+)");
+
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+    compilation.getAllDiagnostics();
+    auto& root = compilation.getRoot();
+
+    TidyConfig config;
+    Registry::setConfig(config);
+    Registry::setSourceManager(compilation.getSourceManager());
+    auto visitor = Registry::create("UnusedSensitiveSignal");
+    bool result = visitor->check(root);
+    CHECK_FALSE(result);
+}
+
+TEST_CASE("UnusedSensitiveSignal: Clock signals matching regex expression won't raise a warning") {
+    auto tree = SyntaxTree::fromText(R"(
+module d_ff_a_en
+(
+    input logic clock_a_i, rst_i, en_i, c_i, d_i,
+
+    output logic q_o
+) ;
+always_ff @ (posedge clock_a_i, posedge rst_i) begin
+    if (rst_i)
+        q_o <= '0;
+    else if (en_i)
+        q_o <= d_i;
+    end
+endmodule
+
+module d_ff_b_en
+(
+    input logic clk_b_i, rst_i, en_i, c_i, d_i,
+
+    output logic q_o
+) ;
+always_ff @ (posedge clk_b_i, posedge rst_i) begin
+    if (rst_i)
+        q_o <= '0;
+    else if (en_i)
+        q_o <= d_i;
+    end
+endmodule
+)");
+
+    Compilation compilation;
+    compilation.addSyntaxTree(tree);
+    compilation.getAllDiagnostics();
+    auto& root = compilation.getRoot();
+
+    TidyConfig config;
+    Registry::setConfig(config);
+    Registry::setSourceManager(compilation.getSourceManager());
+    auto visitor = Registry::create("UnusedSensitiveSignal");
+    bool result = visitor->check(root);
+    CHECK(result);
+}
