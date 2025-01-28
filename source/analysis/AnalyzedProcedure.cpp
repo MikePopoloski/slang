5f9f0db10b2857c8bab94f8ca4072f86fce84df3
Mike Popoloski <mike@popoloski.com>
2025-01-28 10:00:13 -0500

    Initial commit of analysis manager

---
+++ source/analysis/AnalyzedProcedure.cpp
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+// AnalyzedProcedure.cpp
+// Analysis support for procedures
+//
+// SPDX-FileCopyrightText: Michael Popoloski
+// SPDX-License-Identifier: MIT
+//------------------------------------------------------------------------------
+#include "slang/analysis/AnalyzedProcedure.h"
+
+namespace slang::analysis {
+
+using namespace ast;
+
+AnalyzedProcedure::AnalyzedProcedure(AnalysisManager& analysisManager,
+                                     const ProceduralBlockSymbol& symbol) {
+}
+
+} // namespace slang::analysis
