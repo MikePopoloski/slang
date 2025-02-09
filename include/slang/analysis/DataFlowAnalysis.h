6506ee27744deab620e00b2802d8815c10b6f72a
Mike Popoloski <mike@popoloski.com>
2025-02-08 22:04:10 -0500

    analysis: track rvalue ranges in addition to lvalues

--- include/slang/analysis/DataFlowAnalysis.h
+++ include/slang/analysis/DataFlowAnalysis.h
@@ -15,12 +15,12 @@
 
 namespace slang::analysis {
 
-using AssignedBitMap = IntervalMap<uint64_t, std::monostate, 3>;
+using SymbolBitMap = IntervalMap<uint64_t, std::monostate, 3>;
 
 struct SLANG_EXPORT DataFlowState {
     // Each tracked variable has its assigned intervals stored here.
     // This should be 64 bytes per variable.
-    SmallVector<AssignedBitMap, 2> assigned;
+    SmallVector<SymbolBitMap, 2> assigned;
 
     // Whether the control flow that arrived at this point is reachable.
     bool reachable = true;
@@ -36,7 +36,7 @@ class SLANG_EXPORT DataFlowAnalysis : public AbstractFlowAnalysis<DataFlowAnalys
 public:
     /// Constructs a new DataFlowAnalysis object.
     DataFlowAnalysis(AnalysisContext& context, const Symbol& symbol) :
-        AbstractFlowAnalysis(symbol), context(context), assignedBitAllocator(context.alloc) {}
+        AbstractFlowAnalysis(symbol), context(context), bitMapAllocator(context.alloc) {}
 
     /// Gets all of the symbols that are assigned anywhere in the procedure
     /// and aren't definitely assigned by the end of the procedure.
@@ -44,8 +44,8 @@ public:
         SmallVector<std::pair<const Symbol*, const Expression*>>& results) const {
 
         auto& currState = getState();
-        for (size_t index = 0; index < lvalueSymbols.size(); index++) {
-            auto& symbolState = lvalueSymbols[index];
+        for (size_t index = 0; index < lvalues.size(); index++) {
+            auto& symbolState = lvalues[index];
             if (currState.assigned.size() <= index ||
                 !isFullyAssigned(symbolState.assigned, currState.assigned[index])) {
                 results.push_back({symbolState.symbol, symbolState.firstLSP});
@@ -57,7 +57,7 @@ private:
     friend class AbstractFlowAnalysis;
 
     AnalysisContext& context;
-    AssignedBitMap::allocator_type assignedBitAllocator;
+    SymbolBitMap::allocator_type bitMapAllocator;
 
     // Maps visited symbols to slots in assigned vectors.
     SmallMap<const Symbol*, uint32_t, 4> symbolToSlot;
@@ -65,44 +65,156 @@ private:
     // Tracks the assigned ranges of each variable across the entire procedure,
     // even if not all branches assign to it.
     struct LValueSymbol {
-        const Symbol* symbol;
+        const ValueSymbol* symbol;
+        // TODO: track LSP per bit range so we can accurately report
+        // which portions of a symbol are assigned and where.
         const Expression* firstLSP;
-        AssignedBitMap assigned;
+        SymbolBitMap assigned;
 
-        LValueSymbol(const Symbol* symbol, const Expression* firstLSP) :
+        LValueSymbol(const ValueSymbol* symbol, const Expression* firstLSP) :
             symbol(symbol), firstLSP(firstLSP) {}
     };
-    SmallVector<LValueSymbol> lvalueSymbols;
+    SmallVector<LValueSymbol> lvalues;
+
+    // All of the nets and variables that have been read in the procedure.
+    SmallMap<const ValueSymbol*, SymbolBitMap, 4> rvalues;
+
+    // The currently active longest static prefix expression, if there is one.
+    const Expression* currentLSP = nullptr;
+    bool isLValue = false;
+
+    [[nodiscard]] auto saveLValueFlag() {
+        auto guard = ScopeGuard([this, savedLVal = isLValue] { isLValue = savedLVal; });
+        isLValue = false;
+        return guard;
+    }
+
+    bool isFullyAssigned(const SymbolBitMap& left, const SymbolBitMap& right) const {
+        // The left set is the union of everything we've ever assigned
+        // in this procedure, so we only need to check that the right
+        // set is exactly equal to the left set.
+        auto lit = left.begin(), rit = right.begin();
+        auto lend = left.end(), rend = right.end();
+        while (lit != lend || rit != rend) {
+            if (lit == lend || rit == rend)
+                return false;
+
+            if (lit.bounds() != rit.bounds())
+                return false;
+
+            ++lit;
+            ++rit;
+        }
+        return true;
+    }
+
+    // **** AST Handlers ****
+
+    template<typename T>
+        requires(std::is_base_of_v<Expression, T>)
+    void handle(const T& expr) {
+        currentLSP = nullptr;
+        visitExpr(expr);
+    }
 
     void handle(const AssignmentExpression& expr) {
-        visitExpr(expr);
+        // Note that this method mirrors the logic in the base class
+        // handler but we need to track the LValue status of the lhs.
+        SLANG_ASSERT(!isLValue);
+        SLANG_ASSERT(!currentLSP);
+        isLValue = true;
+        visit(expr.left());
+        isLValue = false;
 
-        auto& evalCtx = getEvalContext();
+        if (!expr.isLValueArg()) {
+            if (expr.isCompound()) {
+                auto& binExpr = expr.right().as<BinaryExpression>();
+                SLANG_ASSERT(binExpr.left().kind == ExpressionKind::LValueReference);
+                visit(binExpr.right());
+            }
+            else {
+                visit(expr.right());
+            }
+        }
+    }
+
+    void handle(const ElementSelectExpression& expr) {
+        if (expr.isConstantSelect(getEvalContext())) {
+            if (!currentLSP)
+                currentLSP = &expr;
+        }
+        else {
+            currentLSP = nullptr;
+        }
+
+        visit(expr.value());
+
+        auto guard = saveLValueFlag();
+        visit(expr.selector());
+    }
+
+    void handle(const RangeSelectExpression& expr) {
+        if (expr.isConstantSelect(getEvalContext())) {
+            if (!currentLSP)
+                currentLSP = &expr;
+        }
+        else {
+            currentLSP = nullptr;
+        }
+
+        visit(expr.value());
+
+        auto guard = saveLValueFlag();
+        visit(expr.left());
+        visit(expr.right());
+    }
+
+    void handle(const MemberAccessExpression& expr) {
+        if (!currentLSP)
+            currentLSP = &expr;
+
+        visit(expr.value());
+    }
+
+    void handle(const HierarchicalValueExpression& expr) { recordSymbol(expr); }
+    void handle(const NamedValueExpression& expr) { recordSymbol(expr); }
+
+    void recordSymbol(const ValueExpressionBase& expr) {
+        auto lsp = std::exchange(currentLSP, nullptr);
+        if (!lsp)
+            lsp = &expr;
+
+        // TODO: think harder about whether unreachable symbol references
+        // still count as usages for the procedure.
         auto& currState = getState();
         if (!currState.reachable)
             return;
 
-        SmallVector<std::pair<const ValueSymbol*, const Expression*>> prefixes;
-        expr.left().getLongestStaticPrefixes(prefixes, evalCtx);
+        auto& symbol = expr.symbol;
+        auto bounds = ValueDriver::getBounds(*lsp, getEvalContext(), symbol.getType());
+        if (!bounds)
+            return; // TODO: what cases can get us here?
 
-        for (auto [symbol, prefix] : prefixes) {
-            auto [it, inserted] = symbolToSlot.try_emplace(symbol, (uint32_t)lvalueSymbols.size());
+        if (isLValue) {
+            auto [it, inserted] = symbolToSlot.try_emplace(&symbol, (uint32_t)lvalues.size());
             if (inserted) {
-                lvalueSymbols.emplace_back(symbol, prefix);
-                SLANG_ASSERT(lvalueSymbols.size() == symbolToSlot.size());
+                lvalues.emplace_back(&symbol, &expr);
+                SLANG_ASSERT(lvalues.size() == symbolToSlot.size());
             }
 
             auto index = it->second;
             if (index >= currState.assigned.size())
                 currState.assigned.resize(index + 1);
 
-            auto bounds = ValueDriver::getBounds(*prefix, evalCtx, symbol->getType());
-            if (bounds) {
-                currState.assigned[index].unionWith(*bounds, {}, assignedBitAllocator);
-                lvalueSymbols[index].assigned.unionWith(*bounds, {}, assignedBitAllocator);
-            }
+            currState.assigned[index].unionWith(*bounds, {}, bitMapAllocator);
+            lvalues[index].assigned.unionWith(*bounds, {}, bitMapAllocator);
+        }
+        else {
+            rvalues[&expr.symbol].unionWith(*bounds, {}, bitMapAllocator);
         }
     }
+
+    // **** State Management ****
 
     void joinState(DataFlowState& result, const DataFlowState& other) {
         if (result.reachable == other.reachable) {
@@ -111,7 +223,7 @@ private:
 
             for (size_t i = 0; i < result.assigned.size(); i++) {
                 result.assigned[i] = result.assigned[i].intersection(other.assigned[i],
-                                                                     assignedBitAllocator);
+                                                                     bitMapAllocator);
             }
         }
         else if (!result.reachable) {
@@ -131,7 +243,7 @@ private:
 
         for (size_t i = 0; i < other.assigned.size(); i++) {
             for (auto it = other.assigned[i].begin(); it != other.assigned[i].end(); ++it)
-                result.assigned[i].unionWith(it.bounds(), *it, assignedBitAllocator);
+                result.assigned[i].unionWith(it.bounds(), *it, bitMapAllocator);
         }
     }
 
@@ -140,7 +252,7 @@ private:
         result.reachable = source.reachable;
         result.assigned.reserve(source.assigned.size());
         for (size_t i = 0; i < source.assigned.size(); i++)
-            result.assigned.emplace_back(source.assigned[i].clone(assignedBitAllocator));
+            result.assigned.emplace_back(source.assigned[i].clone(bitMapAllocator));
         return result;
     }
 
@@ -151,25 +263,6 @@ private:
     }
 
     DataFlowState topState() const { return {}; }
-
-    bool isFullyAssigned(const AssignedBitMap& left, const AssignedBitMap& right) const {
-        // The left set is the union of everything we've ever assigned
-        // in this procedure, so we only need to check that the right
-        // set is exactly equal to the left set.
-        auto lit = left.begin(), rit = right.begin();
-        auto lend = left.end(), rend = right.end();
-        while (lit != lend || rit != rend) {
-            if (lit == lend || rit == rend)
-                return false;
-
-            if (lit.bounds() != rit.bounds())
-                return false;
-
-            ++lit;
-            ++rit;
-        }
-        return true;
-    }
 };
 
 } // namespace slang::analysis
