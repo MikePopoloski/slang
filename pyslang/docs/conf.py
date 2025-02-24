fe30a4631fea236858faf8384602ee2e4dec516c
parker-research <166864283+parker-research@users.noreply.github.com>
2025-02-24 15:03:33 -0700

    Add Pyslang homepage content from README, add navbar

--- pyslang/docs/conf.py
+++ pyslang/docs/conf.py
@@ -11,5 +11,19 @@ INPUT_MODULES = ["pyslang"]
 PYBIND11_COMPATIBILITY = True
 OUTPUT = root_dir
 
+INPUT_PAGES = [
+    "pages/index.rst",  # Custom home page. `index.rst` gets used as the homepage by default.
+]
+
+LINKS_NAVBAR1 = [
+    ("Slang Documentation", "/", []),
+    ("GitHub", "https://github.com/MikePopoloski/slang", []),
+    ("PyPI", "https://pypi.org/project/pyslang/", []),
+    # Default links:
+    ("Pages", "pages", []),
+    ("Modules", "modules", []),
+    ("Classes", "classes", []),
+]
+
 # Output the stubs for comparison/review, but not actually used.
 OUTPUT_STUBS = os.path.join(OUTPUT, "stubs")
