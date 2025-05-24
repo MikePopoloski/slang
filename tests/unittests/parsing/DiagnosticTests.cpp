MikePopoloski/slang

commit ae347726db4c7e64a3b60cdc570bfc494c5b732a

Rework warning control options

source/diagnostics/DiagnosticEngine.cpp
@@ -58,11 +58,6 @@ void DiagnosticEngine::setSeverity(DiagCode code, DiagnosticSeverity severity) {
     severityTable[code] = severity;
 }
 
-void DiagnosticEngine::setSeverity(const DiagGroup& group, DiagnosticSeverity severity) {
-    for (auto diag : group.getDiags())
-        setSeverity(diag, severity);
-}
-
 DiagnosticSeverity DiagnosticEngine::getSeverity(DiagCode code, SourceLocation location) const {
     // Check if we have an in-source severity configured.
     if (auto sev = findMappedSeverity(code, location); sev.has_value())
@@ -450,21 +445,37 @@ std::string DiagnosticEngine::reportAll(const SourceManager& sourceManager,
     return client->getString();
 }
 
-void DiagnosticEngine::setDefaultWarnings() {
-    setIgnoreAllWarnings(true);
-    setSeverity(*findDiagGroup("default"sv), DiagnosticSeverity::Warning);
-}
-
 Diagnostics DiagnosticEngine::setWarningOptions(std::span<const std::string> options) {
     Diagnostics diags;
-    auto findAndSet = [&](std::string_view name, DiagnosticSeverity severity,
-                          const char* errorPrefix) {
+    std::vector<std::pair<const DiagGroup*, bool>> groupEnables;
+    std::vector<std::pair<DiagCode, bool>> codeEnables;
+    flat_hash_map<const DiagGroup*, bool> groupErrors;
+    flat_hash_map<DiagCode, bool> codeErrors;
+    bool includeDefault = true;
+
+    auto findAndSet = [&](std::string_view name, bool set, const char* errorPrefix,
+                          bool isExplicitError) {
         if (auto group = findDiagGroup(name)) {
-            setSeverity(*group, severity);
+            if (isExplicitError)
+                groupErrors[group] = set;
+            else
+                groupEnables.emplace_back(group, set);
+
+            // -Werror= also turns on the group.
+            if (isExplicitError && set)
+                groupEnables.emplace_back(group, true);
         }
         else if (auto codes = findFromOptionName(name); !codes.empty()) {
-            for (auto code : codes)
-                setSeverity(code, severity);
+            for (auto code : codes) {
+                if (isExplicitError)
+                    codeErrors[code] = set;
+                else
+                    codeEnables.emplace_back(code, set);
+
+                // -Werror= also turns on the code.
+                if (isExplicitError && set)
+                    codeEnables.emplace_back(code, true);
+            }
         }
         else {
             auto& diag = diags.add(diag::UnknownWarningOption, SourceLocation::NoLocation);
@@ -472,35 +483,72 @@ Diagnostics DiagnosticEngine::setWarningOptions(std::span<const std::string> opt
         }
     };
 
+    // By default all warnings are turned *on*, and we only want the default set by default,
+    // so turn off all warnings here and we'll add the default set at the end.
+    setIgnoreAllWarnings(true);
+
     for (auto& arg : options) {
         if (arg == "everything") {
             // Enable all warnings.
-            clearMappings(DiagnosticSeverity::Ignored);
             setIgnoreAllWarnings(false);
         }
         else if (arg == "none") {
             // Disable all warnings.
-            clearMappings(DiagnosticSeverity::Warning);
+            includeDefault = false;
             setIgnoreAllWarnings(true);
         }
         else if (arg == "error") {
-            for (auto it = severityTable.begin(); it != severityTable.end(); it++) {
-                if (it->second == DiagnosticSeverity::Warning)
-                    it->second = DiagnosticSeverity::Error;
-            }
             setWarningsAsErrors(true);
         }
         else if (arg.starts_with("error=")) {
-            findAndSet(arg.substr(6), DiagnosticSeverity::Error, "-Werror=");
+            findAndSet(arg.substr(6), /* set */ true, "-Werror=", /* isExplicitError */ true);
         }
         else if (arg.starts_with("no-error=")) {
-            findAndSet(arg.substr(9), DiagnosticSeverity::Warning, "-Wno-error=");
+            findAndSet(arg.substr(9), /* set */ false, "-Wno-error=", /* isExplicitError */ true);
         }
         else if (arg.starts_with("no-")) {
-            findAndSet(arg.substr(3), DiagnosticSeverity::Ignored, "-Wno-");
+            findAndSet(arg.substr(3), /* set */ false, "-Wno-", /* isExplicitError */ false);
         }
         else {
-            findAndSet(arg, DiagnosticSeverity::Warning, "-W");
+            findAndSet(arg, /* set */ true, "-W", /* isExplicitError */ false);
+        }
+    }
+
+    auto handleGroup = [&](const DiagGroup* group, bool set) {
+        DiagnosticSeverity severity;
+        if (!set)
+            severity = DiagnosticSeverity::Ignored;
+        else if (auto it = groupErrors.find(group); it != groupErrors.end())
+            severity = it->second ? DiagnosticSeverity::Error : DiagnosticSeverity::Warning;
+        else
+            severity = warningsAsErrors ? DiagnosticSeverity::Error : DiagnosticSeverity::Warning;
+
+        for (auto code : group->getDiags())
+            severityTable[code] = severity;
+    };
+
+    // If they didn't pass "none" then enable the default set of warnings.
+    if (includeDefault) {
+        auto group = findDiagGroup("default"sv);
+        SLANG_ASSERT(group);
+        handleGroup(group, true);
+    }
+
+    // Apply all of the collected settings to the severity table.
+    for (auto& [group, set] : groupEnables)
+        handleGroup(group, set);
+
+    for (auto& [code, set] : codeEnables) {
+        if (!set) {
+            severityTable[code] = DiagnosticSeverity::Ignored;
+        }
+        else if (auto it = codeErrors.find(code); it != codeErrors.end()) {
+            severityTable[code] = it->second ? DiagnosticSeverity::Error
+                                             : DiagnosticSeverity::Warning;
+        }
+        else {
+            severityTable[code] = warningsAsErrors ? DiagnosticSeverity::Error
+                                                   : DiagnosticSeverity::Warning;
         }
     }
 

tests/unittests/parsing/DiagnosticTests.cpp
@@ -476,8 +476,6 @@ TEST_CASE("DiagnosticEngine::setWarningOptions") {
         "empty-stmt"s, "no-extra"s, "asdf"s};
 
     DiagnosticEngine engine(getSourceManager());
-    engine.setDefaultWarnings();
-
     Diagnostics diags = engine.setWarningOptions(options);
     CHECK(diags.size() == 1);
 
@@ -666,4 +664,64 @@ source:9:5: warning: extra ';' has no effect [-Wempty-member]
     ; // warn
     ^
 )");
+}
+
+TEST_CASE("Diagnostic warning option corner cases") {
+    auto createEngine = [](std::vector<std::string> options) {
+        DiagnosticEngine engine(getSourceManager());
+        auto diags = engine.setWarningOptions(options);
+        CHECK(diags.empty());
+        return engine;
+    };
+
+    {
+        auto engine = createEngine({"no-constant-conversion"s, "everything"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Ignored);
+    }
+    {
+        auto engine = createEngine({"constant-conversion"s, "none"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Warning);
+    }
+    {
+        auto engine = createEngine({"error=constant-conversion"s, "none"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Error);
+    }
+    {
+        auto engine = createEngine(
+            {"no-error=constant-conversion"s, "error"s, "constant-conversion"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Warning);
+    }
+    {
+        auto engine = createEngine({"error", "constant-conversion"s, "none"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Error);
+    }
+    {
+        auto engine = createEngine({"constant-conversion"s, "no-conversion"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Warning);
+    }
+    {
+        auto engine = createEngine({"error=constant-conversion"s, "no-error=conversion"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Error);
+    }
+    {
+        auto engine = createEngine({"none"s});
+        CHECK(engine.getSeverity(diag::RealLiteralUnderflow, {}) == DiagnosticSeverity::Ignored);
+    }
+    {
+        auto engine = createEngine({"error=constant-conversion"s, "constant-conversion"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Error);
+    }
+    {
+        auto engine = createEngine({"no-constant-conversion"s, "no-error=constant-conversion"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Ignored);
+    }
+    {
+        auto engine = createEngine(
+            {"error", "no-error=constant-conversion"s, "constant-conversion"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Warning);
+    }
+    {
+        auto engine = createEngine({"no-error=constant-conversion"s});
+        CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Ignored);
+    }
 }

bindings/python/UtilBindings.cpp
@@ -267,13 +267,7 @@ void registerUtil(py::module_& m) {
         .def("setWarningsAsErrors", &DiagnosticEngine::setWarningsAsErrors, "set"_a)
         .def("setErrorsAsFatal", &DiagnosticEngine::setErrorsAsFatal, "set"_a)
         .def("setFatalsAsErrors", &DiagnosticEngine::setFatalsAsErrors, "set"_a)
-        .def("setSeverity",
-             py::overload_cast<DiagCode, DiagnosticSeverity>(&DiagnosticEngine::setSeverity),
-             "code"_a, "severity"_a)
-        .def("setSeverity",
-             py::overload_cast<const DiagGroup&, DiagnosticSeverity>(
-                 &DiagnosticEngine::setSeverity),
-             "group"_a, "severity"_a)
+        .def("setSeverity", &DiagnosticEngine::setSeverity, "code"_a, "severity"_a)
         .def("getSeverity", &DiagnosticEngine::getSeverity, "code"_a, "location"_a)
         .def("setMessage", &DiagnosticEngine::setMessage, "code"_a, "message"_a)
         .def("getMessage", &DiagnosticEngine::getMessage, "code"_a)
@@ -284,7 +278,6 @@ void registerUtil(py::module_& m) {
         .def("clearMappings",
              py::overload_cast<DiagnosticSeverity>(&DiagnosticEngine::clearMappings), "severity"_a)
         .def("formatMessage", &DiagnosticEngine::formatMessage, "diag"_a)
-        .def("setDefaultWarnings", &DiagnosticEngine::setDefaultWarnings)
         .def("setWarningOptions", &DiagnosticEngine::setWarningOptions, "options"_a)
         .def("setMappingsFromPragmas",
              py::overload_cast<>(&DiagnosticEngine::setMappingsFromPragmas))

docs/command-line-ref.dox
@@ -574,33 +574,56 @@ disable the limit. The default is 64.
 
 Don't issue an error for instantiations of unknown modules, interface, and programs.
 
+@section clr-warnings Warnings
+
+Warnings are diagnostic messages that report constructions that are not inherently illegal according to
+the LRM but that are risky or suggest there may have been a mistake in the source. slang has a minimal
+set of warnings enabled by default, which are selected such that running with the default set should
+rarely result in warnings on most codebases. It is recommended that additional warnings be enabled to
+discover potential errors in the design, with the `-Wextra` group being a good recommended starting set,
+but many more are available. See @ref warning-ref for a complete list of warnings that can be enabled.
+
+Some controllable warning names are actually a "group" of warnings that can be enabled or disabled
+all at once. See @ref WarningGroups for a list of such groups.
+
+Which warnings are enabled can be controlled with the options below. Except where noted, options
+later in the command line override those that came earlier if they target the same warning with
+conflicting settings. If an individual warning and a group that also contains that warning are
+given conflicting settings, the option controlling the individual warning will win out,
+regardless of what order it appears in the command line.
+
 `-Wfoo`
 
-Enable warning "foo". See @ref warning-ref for a complete list of warnings that can be enabled this way.
+Enable warning (or group of warnings) "foo".
 
 `-Wno-foo`
 
-Disable warning "foo".
+Disable warning (or group of warnings) "foo".
 
 `-Wnone`
 
-Disable all warnings.
+Disable all warnings. This does not apply to any warnings that have been explicitly controlled by
+another option, either before or after this one in the command line.
 
 `-Weverything`
 
-Enable all warnings.
+Enable all warnings. This does not apply to any warnings that have been explicitly controlled by
+another option, either before or after this one in the command line.
 
 `-Werror`
 
-Treat all warnings as errors.
+Treat all warnings as errors. This only applies to warnings that are enabled by some other option,
+either before or after this one in the command line.
 
 `-Werror=foo`
 
-Treat warning "foo" as an error.
+Treat warning (or group of warnings) "foo" as an error. This also enables the specified warning.
 
 `-Wno-error=foo`
 
-Turn warning "foo" into a warning even if `-Werror` is specified.
+Treat warning (or group of warnings) "foo" as a warning even if `-Werror` is specified, regardless
+of whether `-Werror` appears before or after in the command line. Note that unlike `-Werror=`, this
+will not turn on the warning if it hasn't been enabled with a separate option.
 
 @section clr-profiling Profiling
 

include/slang/diagnostics/DiagnosticEngine.h
@@ -115,9 +115,6 @@ public:
     /// be used to define a new user-specified diagnostic.
     void setSeverity(DiagCode code, DiagnosticSeverity severity);
 
-    /// Sets the severity for all of diagnostics in the given group.
-    void setSeverity(const DiagGroup& group, DiagnosticSeverity severity);
-
     /// Gets the severity currently mapped for the given diagnostic, at the given
     /// location in the source code.
     DiagnosticSeverity getSeverity(DiagCode code, SourceLocation location) const;
@@ -194,9 +191,6 @@ public:
     /// Formats the given diagnostic using its arguments and the currently mapped
     /// message for its diagnostic code.
     std::string formatMessage(const Diagnostic& diag) const;
-
-    /// Initializes diagnostic warnings to the default group.
-    void setDefaultWarnings();
 
     /// Sets diagnostic options from the given option strings, typically from a list of -W
     /// arguments passed to a command line invocation. Any errors encountered while parsing

source/driver/Driver.cpp
@@ -584,7 +584,6 @@ bool Driver::processOptions() {
         tdc.showHierarchyInstance(ShowHierarchyPathOption::Never);
 
     diagEngine.setErrorLimit((int)options.errorLimit.value_or(20));
-    diagEngine.setDefaultWarnings();
 
     // Some tools violate the standard in various ways, but in order to allow
     // compatibility with these tools we change the respective errors into a

commit 944b1fd1ef5f19bbdecd477804b6d9cbe2290d9a

Fix diagnostic control of errors set to initial severities via API (broken recently by ae34772)

source/diagnostics/DiagnosticEngine.cpp
@@ -524,7 +524,7 @@ Diagnostics DiagnosticEngine::setWarningOptions(std::span<const std::string> opt
             severity = warningsAsErrors ? DiagnosticSeverity::Error : DiagnosticSeverity::Warning;
 
         for (auto code : group->getDiags())
-            severityTable[code] = severity;
+            severityTable.try_emplace(code, severity);
     };
 
     // If they didn't pass "none" then enable the default set of warnings.
@@ -537,6 +537,16 @@ Diagnostics DiagnosticEngine::setWarningOptions(std::span<const std::string> opt
     // Apply all of the collected settings to the severity table.
     for (auto& [group, set] : groupEnables)
         handleGroup(group, set);
+
+    // Special case for diagnostics with an explicit severity set by API that
+    // the user is now trying to downgrade to be not an error.
+    for (auto& [code, set] : codeErrors) {
+        if (!set) {
+            auto it = severityTable.find(code);
+            if (it != severityTable.end() && it->second == DiagnosticSeverity::Error)
+                it->second = DiagnosticSeverity::Warning;
+        }
+    }
 
     for (auto& [code, set] : codeEnables) {
         if (!set) {

tests/unittests/parsing/DiagnosticTests.cpp
@@ -669,6 +669,9 @@ source:9:5: warning: extra ';' has no effect [-Wempty-member]
 TEST_CASE("Diagnostic warning option corner cases") {
     auto createEngine = [](std::vector<std::string> options) {
         DiagnosticEngine engine(getSourceManager());
+        engine.setSeverity(diag::UnknownSystemName, DiagnosticSeverity::Error);
+        engine.setSeverity(diag::StaticInitializerMustBeExplicit, DiagnosticSeverity::Ignored);
+
         auto diags = engine.setWarningOptions(options);
         CHECK(diags.empty());
         return engine;
@@ -723,5 +726,25 @@ TEST_CASE("Diagnostic warning option corner cases") {
     {
         auto engine = createEngine({"no-error=constant-conversion"s});
         CHECK(engine.getSeverity(diag::ConstantConversion, {}) == DiagnosticSeverity::Ignored);
+    }
+    {
+        auto engine = createEngine({});
+        CHECK(engine.getSeverity(diag::UnknownSystemName, {}) == DiagnosticSeverity::Error);
+        CHECK(engine.getSeverity(diag::StaticInitializerMustBeExplicit, {}) ==
+              DiagnosticSeverity::Ignored);
+    }
+    {
+        auto engine = createEngine({"none"s});
+        CHECK(engine.getSeverity(diag::UnknownSystemName, {}) == DiagnosticSeverity::Error);
+        CHECK(engine.getSeverity(diag::StaticInitializerMustBeExplicit, {}) ==
+              DiagnosticSeverity::Ignored);
+    }
+    {
+        auto engine = createEngine({"no-error=unknown-sys-name"s});
+        CHECK(engine.getSeverity(diag::UnknownSystemName, {}) == DiagnosticSeverity::Warning);
+    }
+    {
+        auto engine = createEngine({"no-unknown-sys-name"s});
+        CHECK(engine.getSeverity(diag::UnknownSystemName, {}) == DiagnosticSeverity::Ignored);
     }
 }