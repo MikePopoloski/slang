269252d6b17513704f5ba53ab763c81ab8b4d8d7
Catherine <whitequark@whitequark.org>
2025-07-22 18:16:45 +0100

    Address portability to multi-threaded WebAssembly/WASI (#1440)

--- external/boost_unordered.hpp
+++ external/boost_unordered.hpp
@@ -5728,11 +5728,11 @@ struct group15 {
     inline int match(std::size_t hash) const { return match_impl(reduced_hash(hash)); }
 
     inline bool is_not_overflowed(std::size_t hash) const {
-        return !(reinterpret_cast<const boost::uint16_t*>(m)[hash % 8] & 0x8000u);
+        return !(reinterpret_cast<const uint16_t*>(m)[hash % 8] & 0x8000u);
     }
 
     inline void mark_overflow(std::size_t hash) {
-        reinterpret_cast<boost::uint16_t*>(m)[hash % 8] |= 0x8000u;
+        reinterpret_cast<uint16_t*>(m)[hash % 8] |= 0x8000u;
     }
 
     static inline bool maybe_caused_overflow(unsigned char* pc) {
