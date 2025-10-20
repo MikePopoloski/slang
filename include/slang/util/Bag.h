57a3f0941f378781a0e35cebece77a5f2a2e7eae
Andrew Nolte <anolte@hudson-trading.com>
2025-10-20 17:10:33 -0400

    Fix bag footgun (#1545)

--- include/slang/util/Bag.h
+++ include/slang/util/Bag.h
@@ -27,7 +27,9 @@ public:
     Bag() = default;
 
     template<typename... T>
-    Bag(T&&... items) {
+    Bag(T&&... items)
+        requires((!std::is_same_v<std::decay_t<T>, Bag> && ...))
+    {
         (set(std::forward<decltype(items)>(items)), ...);
     }
 
