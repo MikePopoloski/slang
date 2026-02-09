ff9b2953bedd579a1693ea027ae6b44e19d3364c
Chaitanya Sharma <1010chaitanya@gmail.com>
2026-02-09 17:25:56 -0500

    isort and black are fighting

--- pyslang/pyslang/__init__.py
+++ pyslang/pyslang/__init__.py
@@ -1,8 +1,8 @@
 import sys as _sys
 
 from pyslang.pyslang import *  # noqa: F401,F403
-from pyslang.pyslang import (__version__, analysis, ast, driver,  # noqa: F401
-                             parsing, syntax)
+from pyslang.pyslang import driver  # noqa: F401
+from pyslang.pyslang import __version__, analysis, ast, parsing, syntax
 
 for _name, _mod in [
     ("pyslang.ast", ast),
