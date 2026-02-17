b905b36f84c4f676b7e59dc286a50f8ea420e1a5
ilthraim <millerek5@gmail.com>
2026-02-16 20:39:36 -0500

    Added stubs for pyslang imports

--- pyslang/pyslang/__init__.py
+++ pyslang/pyslang/__init__.py
@@ -1,8 +1,12 @@
 import sys as _sys
 
 from pyslang.pyslang import *  # noqa: F401,F403
-from pyslang.pyslang import driver  # noqa: F401
-from pyslang.pyslang import __version__, analysis, ast, parsing, syntax
+from pyslang.pyslang import ast as ast
+from pyslang.pyslang import syntax as syntax
+from pyslang.pyslang import parsing as parsing
+from pyslang.pyslang import analysis as analysis
+from pyslang.pyslang import driver as driver
+from pyslang.pyslang import __version__ as __version__
 
 for _name, _mod in [
     ("pyslang.ast", ast),
