98aedd95ac93bad9bb0a390e4e6ff283913505b2
ilthraim <millerek5@gmail.com>
2026-02-17 11:13:48 -0500

    Switched stubs to __init__.pyi

---
+++ pyslang/pyslang/__init__.pyi
@@ -0,0 +1,6 @@
+from pyslang.pyslang import ast as ast
+from pyslang.pyslang import syntax as syntax
+from pyslang.pyslang import parsing as parsing
+from pyslang.pyslang import analysis as analysis
+from pyslang.pyslang import driver as driver
+from pyslang.pyslang import __version__ as __version__
