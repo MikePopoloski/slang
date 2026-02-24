bff8f2d2594bb076cd524b39ac9e15b552b479d8
Melvin Cardozo <melvin.cardozo@Melvins-Work-MacBook-Pro.local>
2026-02-23 20:41:31 -0800

    Convert out of block method qualifiers to warning in compat mode

--- source/driver/Driver.cpp
+++ source/driver/Driver.cpp
@@ -713,6 +713,7 @@ bool Driver::processOptions() {
         diagEngine.setSeverity(diag::SpecifyPathConditionExpr, DiagnosticSeverity::Error);
         diagEngine.setSeverity(diag::SolveBeforeDisallowed, DiagnosticSeverity::Error);
         diagEngine.setSeverity(diag::DynamicNotProcedural, DiagnosticSeverity::Error);
+        diagEngine.setSeverity(diag::QualifiersOnOutOfBlock, DiagnosticSeverity::Error);
     }
 
     Diagnostics optionDiags = diagEngine.setWarningOptions(options.warningOptions);
