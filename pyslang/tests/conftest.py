711114730d61e2166feeb7ab92c91f12a0dd7a4e
James Hanlon <mail@jameswhanlon.com>
2026-05-07 11:17:39 +0100

    Fix pyslang 'extract_logic_names' example (#1815)
    * Fix pyslang 'extract_logic_names' example
    
    - Handle new namespaces correctly.
    - Add diagnositics handling.
    - Light refactoring to simplify things.
    - Add example to test set.
    
    * Add conftest.py file

---
+++ pyslang/tests/conftest.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: Michael Popoloski
+# SPDX-License-Identifier: MIT
+
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / "examples"))
