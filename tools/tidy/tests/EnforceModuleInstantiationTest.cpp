4d1e69117e590847d085b1a9f8203cf43fd880d1
Nils Wistoff <nwistoff@tenstorrent.com>
2026-09-22 17:43:47 +0200

    tidy: fix EnforceModuleInstantiationPrefix scope
    EnforceModuleInstantiationPrefix currently only checks the module
    instantiations within the toplevel. The subtree remains unchecked. Fix
    this by recursively visiting the instance's childs via `visitDefault`.
    Also, add corresponding tests.

--- tools/tidy/tests/EnforceModuleInstantiationTest.cpp
+++ tools/tidy/tests/EnforceModuleInstantiationTest.cpp
@@ -28,3 +28,59 @@ endmodule
 )");
     CHECK(result);
 }
+
+TEST_CASE("EnforceModuleInstantiationPrefix: Incorrect submodule instantiation prefix") {
+    auto result = runCheckTest("EnforceModuleInstantiationPrefix", R"(
+module subtest ();
+endmodule
+
+module test ();
+    subtest subtest();
+endmodule
+
+module top();
+    test i_test();
+endmodule
+)");
+    CHECK_FALSE(result);
+}
+
+TEST_CASE("EnforceModuleInstantiationPrefix: Correct submodule instantiation prefix") {
+    auto result = runCheckTest("EnforceModuleInstantiationPrefix", R"(
+module subtest ();
+endmodule
+
+module test ();
+    subtest i_subtest();
+endmodule
+
+module top();
+    test i_test();
+endmodule
+)");
+    CHECK(result);
+}
+
+TEST_CASE("EnforceModuleInstantiationPrefix: Incorrect instance array prefix") {
+    auto result = runCheckTest("EnforceModuleInstantiationPrefix", R"(
+module test ();
+endmodule
+
+module top();
+    test test[1:0]();
+endmodule
+)");
+    CHECK_FALSE(result);
+}
+
+TEST_CASE("EnforceModuleInstantiationPrefix: Correct instance array prefix") {
+    auto result = runCheckTest("EnforceModuleInstantiationPrefix", R"(
+module test ();
+endmodule
+
+module top();
+    test i_test[1:0]();
+endmodule
+)");
+    CHECK(result);
+}
