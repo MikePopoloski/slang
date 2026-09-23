baf26c1309663ef25ee52f968242820a7bd08614
Miodrag Milanović <mmicko@gmail.com>
2026-09-23 20:55:25 +0200

    Refactored SourceSnippet to utility (#1977)

---
+++ include/slang/text/SourceSnippet.h
@@ -0,0 +1,46 @@
+//------------------------------------------------------------------------------
+//! @file SourceSnippet.h
+//! @brief Generating highlighted source snippets.
+//
+// SPDX-FileCopyrightText: Michael Popoloski
+// SPDX-License-Identifier: MIT
+//------------------------------------------------------------------------------
+#pragma once
+
+#include <string>
+#include <string_view>
+
+#include "slang/slang_export.h"
+#include "slang/text/SourceLocation.h"
+#include "slang/util/SmallVector.h"
+#include "slang/util/TextStyle.h"
+
+namespace slang {
+
+class FormatBuffer;
+
+/// Utility functions to generate highlighted source snippets.
+class SLANG_EXPORT SourceSnippet {
+public:
+    SourceSnippet(std::string_view sourceLine, uint32_t tabStop,
+                  std::span<const SourceRange> ranges, SourceLocation caretLoc, size_t col,
+                  SmallVectorBase<std::pair<size_t, size_t>>& invalidRanges);
+
+    /// Gets the source snippet line.
+    std::string getSnippetLine() { return snippetLine; }
+    /// Gets the highlighted line.
+    std::string getHighlightLine() { return highlightLine; }
+
+private:
+    void highlightRange(SourceRange range, SourceLocation caretLoc, size_t col,
+                        std::string_view sourceLine);
+    void insertCaret(size_t offset);
+    void trimHighlight();
+    size_t getColumnForByte(size_t b) const;
+
+    SmallVector<int> byteToColumn;
+    std::string snippetLine;
+    std::string highlightLine;
+};
+
+} // namespace slang
